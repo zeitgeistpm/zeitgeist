e0a30e678154bf7cae6bf3c596d39748073c2550
Logan Saether <X@logansaether.com>
2020-11-19 17:35:55 +0100

    Add it to the node

--- zrml/traits/src/swaps.rs
+++ zrml/traits/src/swaps.rs
@@ -1,9 +1,5 @@
-// use codec::FullCodec;
-use sp_runtime::{
-    // traits::{AtLeast32Bit, MaybeSerializeDeserialize},
-    DispatchResult,
-};
-// use sp_std::fmt::Debug;
+use sp_runtime::DispatchResult;
+use sp_std::vec::Vec;
 
 pub trait Swaps<Balance, Hash> {
     fn create_pool(assets: Vec<Hash>, swap_fee: Balance, weights: Vec<u128>) -> DispatchResult;
