48362aa970de69cd9daeaae67fcd1b3ad5a40c6d
Logan Saether <X@logansaether.com>
2021-02-04 19:02:40 +0100

    Upgrade to Substrate v2.0.1

--- zrml/shares/src/lib.rs
+++ zrml/shares/src/lib.rs
@@ -1,20 +1,17 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use codec::{Decode, Encode};
-use frame_support::{
-    decl_error, decl_event, decl_module, decl_storage, Parameter, ensure,
-};
-use frame_support::traits::{Currency, ReservableCurrency, Get, ExistenceRequirement};
+use frame_support::traits::{Currency, ExistenceRequirement, Get, ReservableCurrency};
+use frame_support::{decl_error, decl_event, decl_module, decl_storage, ensure, Parameter};
 use frame_system::ensure_signed;
-use sp_runtime::{
-    traits::{AtLeast32Bit, CheckedAdd, CheckedSub, MaybeSerializeDeserialize, Member, StaticLookup, Zero},
-    DispatchResult, RuntimeDebug, ModuleId,
-};
 use sp_runtime::{traits::AccountIdConversion, SaturatedConversion};
-use sp_std::{
-    cmp,
-	prelude::*,
+use sp_runtime::{
+    traits::{
+        AtLeast32Bit, CheckedAdd, CheckedSub, MaybeSerializeDeserialize, Member, StaticLookup, Zero,
+    },
+    DispatchResult, ModuleId, RuntimeDebug,
 };
+use sp_std::{cmp, prelude::*};
 use zrml_traits::shares::{ReservableShares, Shares, WrapperShares};
 
 #[cfg(test)]
@@ -23,7 +20,8 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-type BalanceOf<T> = <<T as Trait>::Currency as Currency<<T as frame_system::Trait>::AccountId>>::Balance;
+type BalanceOf<T> =
+    <<T as Trait>::Currency as Currency<<T as frame_system::Trait>::AccountId>>::Balance;
 
 #[derive(Encode, Decode, Clone, PartialEq, Eq, Default, RuntimeDebug)]
 pub struct AccountShares<Balance> {
@@ -33,9 +31,9 @@ pub struct AccountShares<Balance> {
 
 pub trait Trait: frame_system::Trait {
     type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
-    
+
     type Balance: Parameter + Member + Copy + MaybeSerializeDeserialize + AtLeast32Bit + Default;
-    
+
     type Currency: ReservableCurrency<Self::AccountId>;
 
     /// The module identifier.
@@ -56,10 +54,10 @@ decl_storage! {
 
 decl_event!(
     pub enum Event<T>
-        where
-            AccountId = <T as frame_system::Trait>::AccountId,
-            Hash = <T as frame_system::Trait>::Hash,
-            Balance = <T as Trait>::Balance,
+    where
+        AccountId = <T as frame_system::Trait>::AccountId,
+        Hash = <T as frame_system::Trait>::Hash,
+        Balance = <T as Trait>::Balance,
     {
         /// Some shares have been transferred. [shares_id, from, to, amount]
         Transferred(Hash, AccountId, AccountId, Balance),
@@ -129,7 +127,6 @@ impl<T: Trait> Module<T> {
     fn get_module_id() -> T::AccountId {
         T::ModuleId::get().into_account()
     }
-
 }
 
 impl<T: Trait> Shares<T::AccountId, T::Balance, T::Hash> for Module<T> {
@@ -159,11 +156,15 @@ impl<T: Trait> Shares<T::AccountId, T::Balance, T::Hash> for Module<T> {
     fn destroy_all(share_id: T::Hash) -> DispatchResult {
         <Accounts<T>>::remove_prefix(share_id);
         <TotalSupply<T>>::remove(share_id);
-        
+
         Ok(())
     }
 
-    fn ensure_can_withdraw(share_id: T::Hash, who: &T::AccountId, amount: Self::Balance) -> DispatchResult {
+    fn ensure_can_withdraw(
+        share_id: T::Hash,
+        who: &T::AccountId,
+        amount: Self::Balance,
+    ) -> DispatchResult {
         if amount.is_zero() {
             return Ok(());
         }
@@ -188,7 +189,12 @@ impl<T: Trait> Shares<T::AccountId, T::Balance, T::Hash> for Module<T> {
         Ok(())
     }
 
-    fn transfer(share_id: T::Hash, from: &T::AccountId, to: &T::AccountId, amount: Self::Balance) -> DispatchResult {
+    fn transfer(
+        share_id: T::Hash,
+        from: &T::AccountId,
+        to: &T::AccountId,
+        amount: Self::Balance,
+    ) -> DispatchResult {
         if amount.is_zero() || from == to {
             return Ok(());
         }
@@ -206,13 +212,15 @@ impl<T: Trait> Shares<T::AccountId, T::Balance, T::Hash> for Module<T> {
 
 impl<T: Trait> ReservableShares<T::AccountId, T::Balance, T::Hash> for Module<T> {
     fn can_reserve(share_id: T::Hash, who: &T::AccountId, value: T::Balance) -> bool {
-        if value.is_zero() { return true }
+        if value.is_zero() {
+            return true;
+        }
 
         Self::free_balance(share_id, who)
             .checked_sub(&value)
-            .map_or(false, |new_balance|
+            .map_or(false, |new_balance| {
                 Self::ensure_can_withdraw(share_id, who, new_balance).is_ok()
-            )
+            })
     }
 
     fn reserved_balance(share_id: T::Hash, who: &T::AccountId) -> T::Balance {
@@ -220,7 +228,9 @@ impl<T: Trait> ReservableShares<T::AccountId, T::Balance, T::Hash> for Module<T>
     }
 
     fn reserve(share_id: T::Hash, who: &T::AccountId, value: T::Balance) -> DispatchResult {
-        if value.is_zero() { return Ok(()) }
+        if value.is_zero() {
+            return Ok(());
+        }
 
         let free = Self::free_balance(share_id, who);
         let reserved = Self::reserved_balance(share_id, who);
@@ -234,7 +244,9 @@ impl<T: Trait> ReservableShares<T::AccountId, T::Balance, T::Hash> for Module<T>
     }
 
     fn unreserve(share_id: T::Hash, who: &T::AccountId, value: T::Balance) -> T::Balance {
-        if value.is_zero() { return Zero::zero() }
+        if value.is_zero() {
+            return Zero::zero();
+        }
 
         let free = Self::free_balance(share_id, who);
         let reserved = Self::reserved_balance(share_id, who);
@@ -259,20 +271,36 @@ impl<T: Trait> WrapperShares<T::AccountId, BalanceOf<T>, T::Hash> for Module<T> 
     }
 
     fn do_wrap_native_currency(who: T::AccountId, amount: BalanceOf<T>) -> DispatchResult {
-        ensure!(T::Currency::free_balance(&who) >= amount.into(), Error::<T>::BalanceTooLow);
+        ensure!(
+            T::Currency::free_balance(&who) >= amount.into(),
+            Error::<T>::BalanceTooLow
+        );
 
         let id = Self::get_native_currency_id();
 
-        T::Currency::transfer(&who, &Self::get_module_id(), amount, ExistenceRequirement::KeepAlive)?;
+        T::Currency::transfer(
+            &who,
+            &Self::get_module_id(),
+            amount,
+            ExistenceRequirement::KeepAlive,
+        )?;
         Self::generate(id, &who, amount.saturated_into().saturated_into())
     }
 
     fn do_unwrap_native_currency(who: T::AccountId, amount: BalanceOf<T>) -> DispatchResult {
         let id = Self::get_native_currency_id();
 
-        ensure!(Self::free_balance(id, &who) >= amount.saturated_into().saturated_into(), Error::<T>::BalanceTooLow);
+        ensure!(
+            Self::free_balance(id, &who) >= amount.saturated_into().saturated_into(),
+            Error::<T>::BalanceTooLow
+        );
 
         Self::destroy(id, &who, amount.saturated_into().saturated_into())?;
-        T::Currency::transfer(&Self::get_module_id(), &who, amount, ExistenceRequirement::AllowDeath)
+        T::Currency::transfer(
+            &Self::get_module_id(),
+            &who,
+            amount,
+            ExistenceRequirement::AllowDeath,
+        )
     }
 }
