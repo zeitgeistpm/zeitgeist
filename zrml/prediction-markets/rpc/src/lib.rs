b0b8bc1e2a68887fdfe20efd4b16b07685ff7543
Caio <c410.f3r@gmail.com>
2021-03-13 15:30:22 -0300

    Format everything and add rustfmt CI check

--- zrml/prediction-markets/rpc/src/lib.rs
+++ zrml/prediction-markets/rpc/src/lib.rs
@@ -5,10 +5,7 @@ use jsonrpc_core::{Error as RpcError, ErrorCode, Result};
 use jsonrpc_derive::rpc;
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::HeaderBackend;
-use sp_runtime::{
-    generic::BlockId,
-    traits::Block as BlockT,
-};
+use sp_runtime::{generic::BlockId, traits::Block as BlockT};
 use std::sync::Arc;
 
 pub use self::gen_client::Client as PmClient;
--- zrml/traits/src/swaps.rs
+++ zrml/traits/src/swaps.rs
@@ -2,5 +2,10 @@ use sp_runtime::DispatchError;
 use sp_std::vec::Vec;
 
 pub trait Swaps<AccountId, Balance, Hash> {
-    fn do_create_pool(creator: AccountId, assets: Vec<Hash>, swap_fee: Balance, weights: Vec<u128>) -> sp_std::result::Result<u128, DispatchError>;
+    fn do_create_pool(
+        creator: AccountId,
+        assets: Vec<Hash>,
+        swap_fee: Balance,
+        weights: Vec<u128>,
+    ) -> sp_std::result::Result<u128, DispatchError>;
 }
