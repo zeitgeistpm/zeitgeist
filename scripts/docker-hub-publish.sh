3e2404f891e43f7dadf57b56cd1e88c662296318
Logan Saether <x@logansaether.com>
2021-05-07 12:17:27 +0200

    Experiments with CI

--- scripts/docker-hub-publish.sh
+++ scripts/docker-hub-publish.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# VERSION=$1
+VERSION=$1
+FEATURES=$2 || "default"
 
-# if [[ -z "$1" ]] ; then
-#     echo "Usage: ./scripts/docker-hub-publish.sh VERSION"
-#     exit 1
-# fi
+if [[ -z "$1" ]] ; then
+    echo "Usage: ./scripts/docker-hub-publish.sh VERSION"
+    exit 1
+fi
 
-# docker build . -t zeitgeistpm/zeitgeist-node:$1 -t zeitgeistpm/zeitgeist-node:latest
-docker build . -t zeitgeistpm/zeitgeist-node:latest
-# docker push zeitgeistpm/zeitgeist-node:$1
-docker push zeitgeistpm/zeitgeist-node:latest
+docker build --build-arg FEATURES=$FEATURES . -t zeitgeistpm/zeitgeist-node-{$2}:$1 -t zeitgeistpm/zeitgeist-node:latest
+docker build --build-arg FEATURES=$FEATURES . -t zeitgeistpm/zeitgeist-node-{$2}:latest
+docker push zeitgeistpm/zeitgeist-node-{$2}:$1
+docker push zeitgeistpm/zeitgeist-node-{$2}:latest
