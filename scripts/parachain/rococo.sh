b545f20a9d339ba2bb4eb17645bd58855365aa6e
Caio <c410.f3r@gmail.com>
2021-05-10 19:23:02 -0300

    Use `node/res/bp_parachain.json` specification for parachain deployment (#124)
    * Use `node/res/bp_parachain.json` specification for parachain deployment
    
    * Enable latest

--- scripts/parachain/rococo.sh
+++ scripts/parachain/rococo.sh
@@ -3,27 +3,26 @@
 # See `testing-network.sh`.
 
 export BOOTNODES=" "
+export DATA_DIR="$HOME/chachacha"
 export DOCKER_POLKADOT_BIN=" "
-export RELAY_CHAIN_SPEC_FILE="/tmp/relay-chain-spec.json"
-export VALIDATOR_CHAIN="rococo"
-
+export PARACHAINS_NUM="2"
 export POLKADOT_IMAGE="parity/rococo:rococo-v1-0.8.30-943038a8-f14fa75f"
+export VALIDATOR_CHAIN="rococo"
+export VALIDATORS_NUM="0"
 
-export PARACHAIN_0="zeitgeist-rococo-parachain-0"
-export PARACHAIN_0_PORT=32000
-export PARACHAIN_0_RPC_PORT=8200
-export PARACHAIN_0_WS_PORT=9200
+export PARACHAIN="zeitgeist-rococo-parachain"
+export PARACHAIN_PORT="32000"
+export PARACHAIN_RPC_PORT="8200"
+export PARACHAIN_WS_PORT="9200"
 
-export VALIDATOR_0="zeitgeist-rococo-validator-0"
-export VALIDATOR_0_PORT=33000
-export VALIDATOR_0_RPC_PORT=8300
-export VALIDATOR_0_WS_PORT=9300
+export VALIDATOR="zeitgeist-rococo-validator"
+export VALIDATOR_PORT="33000"
+export VALIDATOR_RPC_PORT="8300"
+export VALIDATOR_WS_PORT="9300"
 
-export VALIDATOR_1="zeitgeist-rococo-validator-1"
-export VALIDATOR_1_PORT=33001
-export VALIDATOR_1_RPC_PORT=8301
-export VALIDATOR_1_WS_PORT=9301
-
+export PARACHAIN_SPEC_FILE="/tmp/parachain-spec.json"
+curl -o $PARACHAIN_SPEC_FILE https://raw.githubusercontent.com/zeitgeistpm/zeitgeist/main/node/res/bp_parachain.json
+export RELAY_CHAIN_SPEC_FILE="/tmp/relay-chain-spec.json"
 curl -o $RELAY_CHAIN_SPEC_FILE https://raw.githubusercontent.com/paritytech/polkadot/943038a888bfaf736142642e2610b248f7af486c/node/service/res/rococo.json
 
 . "$(dirname "$0")/testing-network.sh" --source-only
