4d80e4d2f10448e371ab3738058f96aaefd6808d
Caio <c410.f3r@gmail.com>
2021-05-29 16:39:33 -0300

    Parallelize clippy on CI

---
+++ scripts/tests/clippy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Clippy with custom permissions
+
+cargo clippy --all-features -- \
+  -Dwarnings \
+  -Aclippy::from_over_into \
+  -Aclippy::let_and_return \
+  -Aclippy::many_single_char_names \
+  -Aclippy::too_many_arguments \
+  -Aclippy::type_complexity \
+  -Aclippy::unnecessary_cast \
+  -Aclippy::unnecessary_mut_passed \
+  -Aclippy::unused_unit
