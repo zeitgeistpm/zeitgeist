2fc7af4f2c3752392dae2ef2c28021fba881e66b
Caio <c410.f3r@gmail.com>
2021-05-31 15:15:30 -0300

    Build optimized binaries on CI (#154)

--- scripts/tests/wasm.sh
+++ scripts/tests/wasm.sh
@@ -6,5 +6,5 @@ set -euxo pipefail
 
 . "$(dirname "$0")/aux-functions.sh" --source-only
 
-WASM_BUILD_TYPE=release cargo build --manifest-path runtime/Cargo.toml
-WASM_BUILD_TYPE=release cargo build --features parachain --manifest-path runtime/Cargo.toml
+WASM_BUILD_TYPE=release cargo build --manifest-path runtime/Cargo.toml --release
+WASM_BUILD_TYPE=release cargo build --features parachain --manifest-path runtime/Cargo.toml --release
