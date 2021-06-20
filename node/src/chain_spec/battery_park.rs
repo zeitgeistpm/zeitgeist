bd81521ed632fa4014fb3150f86b94db50117d12
Caio <c410.f3r@gmail.com>
2021-06-20 20:49:47 -0300

    Remaining fixes for parachain deployment

--- node/src/chain_spec/battery_park.rs
+++ node/src/chain_spec/battery_park.rs
@@ -1,33 +1,32 @@
 use crate::chain_spec::{
-    generic_genesis, AdditionalChainSpec, ChainSpec, POLKADOT_TELEMETRY_URL,
-    ZEITGEIST_TELEMETRY_URL,
+    generic_genesis, telemetry_endpoints, token_properties, zeitgeist_wasm, AdditionalChainSpec,
+    ChainSpec,
 };
 use hex_literal::hex;
-use jsonrpc_core::serde_json::Map;
-use sc_service::{config::TelemetryEndpoints, ChainType};
-#[cfg(not(feature = "parachain"))]
+use sc_service::ChainType;
 use sp_core::crypto::UncheckedInto;
 use zeitgeist_primitives::constants::BASE;
 
 pub fn battery_park_staging_config(
     #[cfg(feature = "parachain")] parachain_id: cumulus_primitives_core::ParaId,
 ) -> Result<ChainSpec, String> {
-    let wasm_binary = zeitgeist_runtime::WASM_BINARY
-        .ok_or_else(|| "Development wasm binary not available".to_string())?;
-
-    let mut properties = Map::new();
-    properties.insert("tokenSymbol".into(), "ZBP".into());
-    properties.insert("tokenDecimals".into(), 10.into());
+    let zeitgeist_wasm = zeitgeist_wasm()?;
 
     Ok(ChainSpec::from_genesis(
-        "Zeitgeist Battery Park Staging",
+        "Zeitgeist - Battery Park Staging",
         "battery_park_staging",
         ChainType::Live,
         move || {
             generic_genesis(
                 #[cfg(feature = "parachain")]
                 AdditionalChainSpec {
-                    candidates: vec![],
+                    candidates: vec![(
+                        hex!["7225b9e36cc33da17d7f97053c7110559f21967afc09867345367b7de241d03c"]
+                            .into(),
+                        hex!["e6ea0b63b2b5b7247a1e8280350a14c5f9e7745dec2fe3428b68aa4167d48e66"]
+                            .unchecked_into(),
+                        crate::chain_spec::DEFAULT_STAKING_AMOUNT,
+                    )],
                     inflation_info: crate::chain_spec::DEFAULT_COLLATOR_INFLATION_INFO,
                     nominations: vec![],
                     parachain_id,
@@ -48,20 +47,18 @@ pub fn battery_park_staging_config(
                     hex!["2a6c61a907556e4c673880b5767dd4be08339ee7f2a58d5137d0c19ca9570a5c"].into(),
                     // 5EeeZVU4SiPG6ZRY7o8aDcav2p2mZMdu3ZLzbREWuHktYdhX
                     hex!["725bb6fd13d52b3d6830e5a9faed1f6499ca0f5e8aa285df09490646e71e831b"].into(),
+                    // 5EeNXHgaiWZAwZuZdDndJYcRTKuGHkXM2bdGE6LqWCw1bHW7
+                    hex!["7225b9e36cc33da17d7f97053c7110559f21967afc09867345367b7de241d03c"].into(),
                 ],
                 10_000 * BASE,
                 hex!["2a6c61a907556e4c673880b5767dd4be08339ee7f2a58d5137d0c19ca9570a5c"].into(),
-                wasm_binary,
+                zeitgeist_wasm,
             )
         },
         vec![],
-        TelemetryEndpoints::new(vec![
-            (POLKADOT_TELEMETRY_URL.into(), 0),
-            (ZEITGEIST_TELEMETRY_URL.into(), 0),
-        ])
-        .ok(),
+        telemetry_endpoints(),
         Some("battery_park_staging"),
-        Some(properties),
+        Some(token_properties()),
         #[cfg(feature = "parachain")]
         crate::chain_spec::Extensions {
             relay_chain: "rococo".into(),
