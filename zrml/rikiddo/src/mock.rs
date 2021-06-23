9c211d57bcf7a4341137bd39af6b0877fa6e87fd
Harald Heckmann <mail@haraldheckmann.de>
2021-06-23 20:33:18 +0200

    Rename rikiddo to zrml-rikiddo

--- zrml/rikiddo/src/mock.rs
+++ zrml/rikiddo/src/mock.rs
@@ -1,9 +1,5 @@
 #![cfg(test)]
-<<<<<<< HEAD
 use crate as zrml_rikiddo;
-=======
-use crate as rikiddo;
->>>>>>> Rename to rikiddo and bump version
 use frame_support::{construct_runtime, parameter_types};
 use sp_runtime::{
     testing::Header,
