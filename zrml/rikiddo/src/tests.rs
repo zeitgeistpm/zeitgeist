fdf9c5ddbe1135bc6506ccd5702851b5a7dcb8d7
Harald Heckmann <mail@haraldheckmann.de>
2021-06-25 21:47:53 +0200

    Change constant types and adjust default functions

--- zrml/rikiddo/src/tests.rs
+++ zrml/rikiddo/src/tests.rs
@@ -2,6 +2,8 @@
 
 use crate::mock::*;
 
+// TODO: Test fee calculation + different overflow scenarios + default values
+
 #[test]
 fn it_is_a_dummy_test() {
     ExtBuilder::default().build().execute_with(|| {
--- zrml/rikiddo/src/types.rs
+++ zrml/rikiddo/src/types.rs
@@ -4,7 +4,12 @@ use crate::{
 };
 use frame_support::dispatch::{fmt::Debug, Decode, Encode};
 use sp_std::marker::PhantomData;
-use substrate_fixed::{FixedI128, FixedU128, FixedU32, traits::{FixedSigned, FixedUnsigned}, transcendental::sqrt, types::extra::{U24, U32, U64}};
+use substrate_fixed::{
+    traits::{FixedSigned, FixedUnsigned},
+    transcendental::sqrt,
+    types::extra::U64,
+    FixedI128,
+};
 
 pub type UnixTimestamp = u64;
 
@@ -36,7 +41,14 @@ pub struct FeeSigmoidConfig {
 
 impl Default for FeeSigmoidConfig {
     fn default() -> Self {
-        Self { m: M, p: P, n: N }
+        // This is a bit hacky: We convert U128 to I128 and reset the MSB.
+        // The config values should never take such enormous values anyways.
+        let bit_mask: u128 = !(1 << 127);
+        Self {
+            m: <FixedI128<U64>>::from_bits((M.to_bits() & bit_mask) as i128),
+            p: <FixedI128<U64>>::from_bits((P.to_bits() & bit_mask) as i128),
+            n: <FixedI128<U64>>::from_bits((N.to_bits() & bit_mask) as i128),
+        }
     }
 }
 
@@ -49,37 +61,37 @@ impl<FI: FixedSigned + Into<FixedI128<U64>>> LsdlmsrFee<FI> for FeeSigmoid {
     type Output = FixedI128<U64>;
 
     // z(r) in https://files.kyber.network/DMM-Feb21.pdf
-    fn calculate(&self, r: FI) -> Result<Self::Output, &'static str>{
-        let r_minus_n = if let Some(res) = r.into().checked_sub(self.config.n)  {
+    fn calculate(&self, r: FI) -> Result<Self::Output, &'static str> {
+        let r_minus_n = if let Some(res) = r.into().checked_sub(self.config.n) {
             res
         } else {
             return Err("[FeeSigmoid] Overflow during calculation: r - n");
         };
 
-        let numerator = if let Some(res) = r_minus_n.checked_mul(self.config.m)  {
+        let numerator = if let Some(res) = r_minus_n.checked_mul(self.config.m) {
             res
         } else {
             return Err("[FeeSigmoid] Overflow during calculation: m * (r-n)");
         };
 
-        let r_minus_n_squared =  if let Some(res) = r_minus_n.checked_mul(r_minus_n)  {
+        let r_minus_n_squared = if let Some(res) = r_minus_n.checked_mul(r_minus_n) {
             res
         } else {
             return Err("[FeeSigmoid] Overflow during calculation: (r-n)^2");
         };
 
-        let p_plus_r_minus_n = if let Some(res) = self.config.p.checked_add(r_minus_n_squared)  {
+        let p_plus_r_minus_n = if let Some(res) = self.config.p.checked_add(r_minus_n_squared) {
             res
         } else {
             return Err("[FeeSigmoid] Overflow during calculation: p + (r-n)^2");
         };
 
-        let denominator = sqrt::<FixedI128<U64>,FixedI128<U64>>(p_plus_r_minus_n)?;
+        let denominator = sqrt::<FixedI128<U64>, FixedI128<U64>>(p_plus_r_minus_n)?;
 
-        let _ = if let Some(res) = numerator.checked_div(denominator)  {
+        let _ = if let Some(res) = numerator.checked_div(denominator) {
             return Ok(res);
         } else {
-            return Err("[FeeSigmoid] Overflow during calculation: numerator / denumerator");
+            return Err("[FeeSigmoid] Overflow during calculation: numerator / denominator");
         };
     }
 }
@@ -87,11 +99,11 @@ impl<FI: FixedSigned + Into<FixedI128<U64>>> LsdlmsrFee<FI> for FeeSigmoid {
 #[derive(Clone, Debug, Decode, Encode, Eq, PartialEq)]
 pub struct EmaVolumeConfig {
     pub ema_period: Timespan,
-    pub multiplier: FixedU32<U24>,
+    pub multiplier: FixedI128<U64>,
 }
 
 impl EmaVolumeConfig {
-    pub fn new(ema_period: Timespan, smoothing: FixedU32<U24>) -> Self {
+    pub fn new(ema_period: Timespan, smoothing: FixedI128<U64>) -> Self {
         let duration: u32 = match ema_period {
             Timespan::Minutes(d) => d,
             Timespan::Hours(d) => d,
@@ -99,8 +111,8 @@ impl EmaVolumeConfig {
             Timespan::Weeks(d) => d.into(),
         };
 
-        let one = FixedU32::<U24>::from_num(1);
-        let fduration = FixedU32::<U24>::from_num(duration);
+        let one = FixedI128::<U64>::from_num(1);
+        let fduration = FixedI128::<U64>::from_num(duration);
 
         Self { ema_period, multiplier: smoothing / (one + fduration) }
     }
@@ -108,7 +120,10 @@ impl EmaVolumeConfig {
 
 impl Default for EmaVolumeConfig {
     fn default() -> Self {
-        Self::new(EMA_SHORT, SMOOTHING)
+        // This is a bit hacky: We convert U128 to I128 and reset the MSB.
+        // The config values should never take such enormous values anyways.
+        let bit_mask: u128 = !(1 << 127);
+        Self::new(EMA_SHORT, <FixedI128<U64>>::from_bits((SMOOTHING.to_bits() & bit_mask) as i128))
     }
 }
 
