09b6e820b9d140490fe454820cccd4804f5965ec
Caio <c410.f3r@gmail.com>
2021-07-13 19:03:49 -0300

    Update DockerHub image

--- scripts/parachain/testing-network.sh
+++ scripts/parachain/testing-network.sh
@@ -14,7 +14,7 @@ set -euxo pipefail
 
 # Variables
 
-PARACHAIN_IMAGE="${PARACHAIN_IMAGE:-zeitgeistpm/zeitgeist-node-parachain:sha-116eba1}"
+PARACHAIN_IMAGE="${PARACHAIN_IMAGE:-zeitgeistpm/zeitgeist-node-parachain:sha-f6941ab}"
 # Minus one because `seq` is inclusive
 PARACHAINS_NUM=$(($PARACHAINS_NUM - 1))
 # Minus one because `seq` is inclusive
