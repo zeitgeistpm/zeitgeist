96c11004d53f671399840c4b2f07016f31c14e28
Harald Heckmann <mail@haraldheckmann.de>
2021-07-19 10:29:28 +0200

    [Rikiddo] Implement Rikiddo helper functions (#199)
    * Add LSD-LSMR skeleton
    
    * Add types and trait functions
    
    * Rename to rikiddo and bump version
    
    * Rename rikiddo to zrml-rikiddo
    
    * Start implementation of FeeSigmoid
    
    * Add z(r) fee calculation
    
    Also updated traits and constants to use different types
    
    * Change constant types and adjust default functions
    
    * Add "valid sigmoid fee result" test
    
    * Add FeeSigmoid overflow tests
    
    * Add self to EmaVolume trait functions
    
    * Update EmaMarketVolume to support multiple ema chains
    
    * Implement default, clear and update for EmaMarketVolume
    
    * Change fee calculation type to generic type
    
    * Implement EmaMarketVolume
    
    * Rename remaining old names to Rikiddo
    
    * Prepare library for better error handling
    
    * Implement arithmetic checks for EmaMarketVolume
    
    * Add generic type for FeeSigmoidConfig
    
    * Add EmaMarketVolume test scaffold
    
    * Implement EmaMarketVolume state transit test
    
    * Fix off-by-one error and add test
    
    * Partially implement EmaMarketVolume tests
    
    * Revert constant types back to 32bit numbers
    
    This decision was made, because the fee and ema structures are limited by the length of the constants. To allow any fixed point numbers with at least 32 bit width, the constants were reverted back to 32 bit length
    
    * Fix missing assignment and implement some tests
    
    * Implement last timestamp check and test
    
    * Implement some overflow tests for ema calculation
    
    * Improve module hierarchy
    
    * Add additional (probably unnecessary) overflow checks
    
    * Remove rebase artifacts
    
    * Clippy
    
    * Use integer sign only when needed

--- zrml/rikiddo/src/types.rs
+++ zrml/rikiddo/src/types.rs
@@ -1,19 +1,25 @@
-use crate::{
-    constants::*,
-    traits::{LsdlmsrFee, MarketAverage},
-};
 use frame_support::dispatch::{fmt::Debug, Decode, Encode};
-use sp_std::marker::PhantomData;
-use substrate_fixed::{
-    traits::FixedUnsigned,
-    types::extra::{U24, U32},
-    FixedU32,
-};
+use substrate_fixed::traits::Fixed;
+
+// use crate::traits::{MarketAverage, Sigmoid};
+
+mod ema_market_volume;
+mod sigmoid_fee;
+
+pub use ema_market_volume::*;
+pub use sigmoid_fee::*;
 
 pub type UnixTimestamp = u64;
 
 #[derive(Clone, Debug, Decode, Encode, Eq, PartialEq)]
+pub struct TimestampedVolume<F: Fixed> {
+    pub timestamp: UnixTimestamp,
+    pub volume: F,
+}
+
+#[derive(Clone, Debug, Decode, Encode, Eq, PartialEq)]
 pub enum Timespan {
+    Seconds(u32),
     Minutes(u32),
     Hours(u32),
     Days(u16),
@@ -21,89 +27,23 @@ pub enum Timespan {
 }
 
 impl Timespan {
-    pub fn into_seconds(timespan: Timespan) -> u64 {
-        match timespan {
-            Timespan::Minutes(d) => u64::from(d) * 60,
-            Timespan::Hours(d) => u64::from(d) * 60 * 60,
-            Timespan::Days(d) => u64::from(d) * 60 * 60 * 24,
-            Timespan::Weeks(d) => u64::from(d) * 60 * 60 * 24 * 7,
+    pub fn to_seconds(&self) -> u32 {
+        match *self {
+            Timespan::Seconds(d) => d,
+            Timespan::Minutes(d) => d * 60,
+            Timespan::Hours(d) => d * 60 * 60,
+            Timespan::Days(d) => u32::from(d) * 60 * 60 * 24,
+            Timespan::Weeks(d) => u32::from(d) * 60 * 60 * 24 * 7,
         }
     }
 }
 
+/*
 #[derive(Clone, Debug, Decode, Encode, Eq, PartialEq)]
-pub struct FeeSigmoidConfig {
-    pub initial_fee: FixedU32<U32>,
-    pub minimal_revenue: FixedU32<U32>,
-    pub m: FixedU32<U24>,
-    pub p: FixedU32<U24>,
-    pub n: FixedU32<U24>,
-}
-
-impl Default for FeeSigmoidConfig {
-    fn default() -> Self {
-        Self { initial_fee: INITIAL_FEE, minimal_revenue: MINIMAL_REVENUE, m: M, p: P, n: N }
-    }
-}
-
-#[derive(Clone, Debug, Decode, Encode, Eq, PartialEq)]
-pub struct FeeSigmoid {
-    pub config: FeeSigmoidConfig,
-}
-
-#[derive(Clone, Debug, Decode, Encode, Eq, PartialEq)]
-pub struct EmaVolumeConfig {
-    pub ema_period: Timespan,
-    pub multiplier: FixedU32<U24>,
-}
-
-impl EmaVolumeConfig {
-    pub fn new(ema_period: Timespan, smoothing: FixedU32<U24>) -> Self {
-        let duration: u32 = match ema_period {
-            Timespan::Minutes(d) => d,
-            Timespan::Hours(d) => d,
-            Timespan::Days(d) => d.into(),
-            Timespan::Weeks(d) => d.into(),
-        };
-
-        let one = FixedU32::<U24>::from_num(1);
-        let fduration = FixedU32::<U24>::from_num(duration);
-
-        Self { ema_period, multiplier: smoothing / (one + fduration) }
-    }
-}
-
-impl Default for EmaVolumeConfig {
-    fn default() -> Self {
-        Self::new(EMA_SHORT, SMOOTHING)
-    }
-}
-
-#[derive(Clone, Debug, Decode, Encode, Eq, PartialEq)]
-pub struct EmaMarketVolume<FI: FixedUnsigned> {
-    pub config: EmaVolumeConfig,
-    sma_current_period: FI,
-    sma_current_element_count: u64,
-    sma_current_period_start: Option<UnixTimestamp>,
-    pub ema: Option<FI>,
-}
-
-impl<FI: FixedUnsigned> EmaMarketVolume<FI> {
-    pub fn new(config: EmaVolumeConfig) -> Self {
-        Self {
-            config,
-            sma_current_period: FI::from_num(0),
-            sma_current_element_count: 0,
-            sma_current_period_start: None,
-            ema: None,
-        }
-    }
-}
-
-#[derive(Clone, Debug, Decode, Encode, Eq, PartialEq)]
-pub struct LsdLmsrSigmoidMV<FI: FixedUnsigned, FE: LsdlmsrFee<FI>, MA: MarketAverage<FI>> {
+pub struct RikiddoSigmoidMV<FI: Fixed, FE: Sigmoid<FI>, MA: MarketAverage<FI>> {
     pub fees: FE,
     pub ma_short: MA,
     pub ma_long: MA,
     pub _marker: PhantomData<FI>,
 }
+*/
