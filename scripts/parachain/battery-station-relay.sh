0946f0d8697aa5a57827b9322355afa449009915
Caio <c410.f3r@gmail.com>
2021-08-13 22:37:52 -0300

    Battery Station Parachain

---
+++ scripts/parachain/battery-station-relay.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# ***** Secrets *****
+#
+# Do not commit any secret
+#
+export OVERALL_SECRET=""
+export PARACHAIN_FIRST_BOOTNODE_NODE_KEY=""
+export PARACHAIN_NIMBUS_SEED=""
+export VALIDATOR_FIRST_BOOTNODE_NODE_KEY=""
+export VALIDATOR_SECOND_BOOTNODE_NODE_KEY=""
+#
+# Do not commit any secret
+#
+# ***** Secrets *****
+
+export DATA_DIR="$HOME/battery-station-relay"
+export DOCKER_POLKADOT_BIN="/usr/local/bin/polkadot"
+
+export PARACHAIN_CHAIN="battery_station"
+export PARACHAIN_FIRST_BOOTNODE_ADDR="/ip4/45.33.117.205/tcp/30001/p2p/12D3KooWBMSGsvMa2A7A9PA2CptRFg9UFaWmNgcaXRxr1pE1jbe9"
+export PARACHAIN_ID="2050"
+export PARACHAIN_IMAGE="zeitgeistpm/zeitgeist-node-parachain:sha-c6bc710"
+export PARACHAIN_NIMBUS_PK="0xe6ea0b63b2b5b7247a1e8280350a14c5f9e7745dec2fe3428b68aa4167d48e66"
+export PARACHAIN_PORT="30000"
+export PARACHAIN_RPC_PORT="8000"
+export PARACHAIN_WS_PORT="9000"
+export PARACHAIN="zeitgeist-battery-station-relay-parachain"
+
+export VALIDATOR_FIRST_BOOTNODE_ADDR="/ip4/45.33.117.205/tcp/31001/p2p/12D3KooWHgbvdWFwNQiUPbqncwPmGCHKE8gUQLbzbCzaVbkJ1crJ"
+export VALIDATOR_IMAGE="zeitgeistpm/zeitgeist-relay-chain:sha-44d53aa"
+export VALIDATOR_PORT="31000"
+export VALIDATOR_RPC_PORT="8100"
+export VALIDATOR_SECOND_BOOTNODE_ADDR="/ip4/45.33.117.205/tcp/31002/p2p/12D3KooWE5KxMrfJLWCpaJmAPLWDm9rS612VcZg2JP6AYgxrGuuE"
+export VALIDATOR_WS_PORT="9100"
+export VALIDATOR="zeitgeist-battery-station-relay-validator"
+
+export RELAY_CHAIN_SPEC_FILE="/tmp/relay-chain-spec.json"
+curl -o $RELAY_CHAIN_SPEC_FILE https://raw.githubusercontent.com/zeitgeistpm/polkadot/battery-station-relay/node/service/res/battery-station-relay.json
+
+. "$(dirname "$0")/testing-network-commons.sh" --source-only
---
+++ scripts/parachain/testing-network-commons.sh
@@ -0,0 +1,209 @@
+#!/usr/bin/env bash
+
+# Not meant to be called directly.
+#
+# Common script used by live testing networks (Rococo, Chachacha, Battery Station Relay, etc). Spin-ups
+# 3 validators and 2 parachains using Docker.
+#
+# First parachain and relay chain nodes will always be a light node while the remaining nodes will be actual validators.
+
+# Generic Functions
+
+delete_container() {
+    local container_name=$1
+
+    sudo docker stop $container_name
+    sudo docker rm $container_name
+}
+
+generate_account_id() {
+    sudo docker run --rm $VALIDATOR_IMAGE $DOCKER_POLKADOT_BIN key inspect ${3:-} "$OVERALL_SECRET//$1//$2" | grep "Account ID" | awk '{ print $3 }'
+}
+
+generate_author_insertKey_with_account_id() {
+    ACCOUNT=$(generate_account_id $1 $2 "$3")
+    SEED=$OVERALL_SECRET//$1//$2
+
+    printf '{"jsonrpc":"2.0","id":1,"method":"author_insertKey","params":["'"$4"'","'"$SEED"'","'"$ACCOUNT"'"]}'
+}
+
+generate_author_insertKey_with_public_key() {
+    PUBLIC_KEY=$(generate_public_key $1 $2 "$3")
+    SEED=$OVERALL_SECRET//$1//$2
+
+    printf '{"jsonrpc":"2.0","id":1,"method":"author_insertKey","params":["'"$4"'","'"$SEED"'","'"$PUBLIC_KEY"'"]}'
+}
+
+generate_public_key() {
+    sudo docker run --rm $VALIDATOR_IMAGE $DOCKER_POLKADOT_BIN key inspect ${3:-} "$OVERALL_SECRET//$1//$2" | grep "Public key (hex)" | awk '{ print $4 }'
+}
+
+initial_container_configurations() {
+    local container_name=$1
+
+    mkdir -p $DATA_DIR/$container_name
+}
+
+# Init
+
+sudo apt update
+sudo apt install -y curl docker.io
+sudo docker pull $PARACHAIN_IMAGE
+sudo docker pull $VALIDATOR_IMAGE
+
+sudo docker container stop $(sudo docker container ls -aq --filter name=$PARACHAIN*) &> /dev/null || true
+sudo docker container stop $(sudo docker container ls -aq --filter name=$VALIDATOR*) &> /dev/null || true
+
+sudo docker container rm $(sudo docker container ls -aq --filter name=$PARACHAIN*) &> /dev/null || true
+sudo docker container rm $(sudo docker container ls -aq --filter name=$VALIDATOR*) &> /dev/null || true
+
+mkdir -p $DATA_DIR
+
+cp $RELAY_CHAIN_SPEC_FILE $DATA_DIR/relay-chain-spec.json
+
+# Validators
+
+inject_keys() {
+    local idx=$1
+    local rpc_port=$2
+
+    curl http://localhost:$rpc_port -H "Content-Type:application/json;charset=utf-8" -d "$(generate_author_insertKey_with_account_id $idx babe '--scheme sr25519' babe)"
+    curl http://localhost:$rpc_port -H "Content-Type:application/json;charset=utf-8" -d "$(generate_author_insertKey_with_account_id $idx grandpa '--scheme ed25519' gran)"
+    curl http://localhost:$rpc_port -H "Content-Type:application/json;charset=utf-8" -d "$(generate_author_insertKey_with_account_id $idx im_online '--scheme sr25519' imon)"
+    curl http://localhost:$rpc_port -H "Content-Type:application/json;charset=utf-8" -d "$(generate_author_insertKey_with_account_id $idx para_validator '--scheme sr25519' para)"
+    curl http://localhost:$rpc_port -H "Content-Type:application/json;charset=utf-8" -d "$(generate_author_insertKey_with_account_id $idx para_assignment '--scheme sr25519' asgn)"
+    curl http://localhost:$rpc_port -H "Content-Type:application/json;charset=utf-8" -d "$(generate_author_insertKey_with_account_id $idx authority_discovery '--scheme sr25519' audi)"
+    curl http://localhost:$rpc_port -H "Content-Type:application/json;charset=utf-8" -d "$(generate_author_insertKey_with_public_key $idx beefy '--scheme ecdsa' beef)"
+}
+
+launch_validator() {
+    local container_name=$1
+    local docker_extra_params=$2
+    local validator_extra_params=$3
+
+    sudo docker run \
+        -d \
+        $docker_extra_params \
+        -v $DATA_DIR/relay-chain-spec.json:/zeitgeist/relay-chain-spec.json \
+        -v $DATA_DIR/$container_name:/data \
+        --name=$container_name \
+        --restart=always \
+        $VALIDATOR_IMAGE \
+        $DOCKER_POLKADOT_BIN \
+        --base-path=/data \
+        --chain=/zeitgeist/relay-chain-spec.json \
+        --name=$container_name \
+        $validator_extra_params
+}
+
+launch_configured_validator() {
+    local idx=$1
+    
+    LOCAL_CONTAINER_NAME="$VALIDATOR-$idx"
+    LOCAL_PORT=$(($VALIDATOR_PORT + $idx))
+    LOCAL_RPC_PORT=$(($VALIDATOR_RPC_PORT + $idx))
+    LOCAL_WS_PORT=$(($VALIDATOR_WS_PORT + $idx))
+
+    initial_container_configurations $LOCAL_CONTAINER_NAME
+
+    DOCKER_EXTRA_PARAMS="-p $LOCAL_PORT:30333 -p $LOCAL_RPC_PORT:9933 -p $LOCAL_WS_PORT:9944"
+
+    if (( $idx <= 0 ))
+    then
+        launch_validator "$LOCAL_CONTAINER_NAME" "$DOCKER_EXTRA_PARAMS" "--bootnodes=$VALIDATOR_FIRST_BOOTNODE_ADDR --bootnodes=$VALIDATOR_SECOND_BOOTNODE_ADDR  --rpc-cors=all --rpc-external --ws-external"
+    else
+        launch_validator $LOCAL_CONTAINER_NAME "$DOCKER_EXTRA_PARAMS" "--rpc-cors=all --rpc-methods=Unsafe --unsafe-rpc-external --validator"
+        sleep 10
+        inject_keys $idx $LOCAL_RPC_PORT
+        delete_container "$LOCAL_CONTAINER_NAME"
+
+        if (( $idx == 1 ))
+        then
+            launch_validator $LOCAL_CONTAINER_NAME "$DOCKER_EXTRA_PARAMS" "--bootnodes=$VALIDATOR_SECOND_BOOTNODE_ADDR --node-key=$VALIDATOR_FIRST_BOOTNODE_NODE_KEY --validator"
+        elif (( $idx == 2 ))
+        then
+            launch_validator $LOCAL_CONTAINER_NAME "$DOCKER_EXTRA_PARAMS" "--bootnodes=$VALIDATOR_FIRST_BOOTNODE_ADDR --node-key=$VALIDATOR_SECOND_BOOTNODE_NODE_KEY --validator"
+        else
+            launch_validator $LOCAL_CONTAINER_NAME "$DOCKER_EXTRA_PARAMS" "--bootnodes=$VALIDATOR_FIRST_BOOTNODE_ADDR --bootnodes=$VALIDATOR_SECOND_BOOTNODE_ADDR --validator"
+        fi
+    fi
+}
+
+launch_configured_validator 0
+launch_configured_validator 1
+launch_configured_validator 2
+
+# Parachains
+
+sudo docker run \
+    --rm \
+    $PARACHAIN_IMAGE \
+    export-genesis-state \
+    --chain=$PARACHAIN_CHAIN \
+    --parachain-id=$PARACHAIN_ID > $DATA_DIR/zeitgeist-genesis-state
+
+sudo docker run \
+    --rm \
+    $PARACHAIN_IMAGE \
+    export-genesis-wasm \
+    --chain=$PARACHAIN_CHAIN > $DATA_DIR/zeitgeist-genesis-wasm
+
+launch_parachain() {
+    local container_name=$1
+    local docker_extra_params=$2
+    local parachain_extra_params=$3
+
+    sudo docker run \
+        -d \
+        $docker_extra_params \
+        -v $DATA_DIR/$LOCAL_CONTAINER_NAME:/zeitgeist/data \
+        -v $DATA_DIR/relay-chain-spec.json:/zeitgeist/relay-chain-spec.json \
+        --name $container_name \
+        --restart always \
+        $PARACHAIN_IMAGE \
+        --base-path=/zeitgeist/data \
+        --chain=$PARACHAIN_CHAIN \
+        --parachain-id=$PARACHAIN_ID \
+        $parachain_extra_params \
+        -- \
+        --bootnodes=$VALIDATOR_FIRST_BOOTNODE_ADDR \
+        --bootnodes=$VALIDATOR_SECOND_BOOTNODE_ADDR \
+        --chain=/zeitgeist/relay-chain-spec.json \
+        --execution=wasm
+}
+
+launch_configured_parachain() {
+    local idx=$1
+    local nimbus_seed=$2
+    local nimbus_pk=$3
+
+    LOCAL_CONTAINER_NAME="$PARACHAIN-$idx"
+    LOCAL_PORT=$(($PARACHAIN_PORT + $idx))
+    LOCAL_RPC_PORT=$(($PARACHAIN_RPC_PORT + $idx))
+    LOCAL_WS_PORT=$(($PARACHAIN_WS_PORT + $idx))
+
+    initial_container_configurations $LOCAL_CONTAINER_NAME
+
+    DOCKER_EXTRA_PARAMS="-p $LOCAL_PORT:30333 -p $LOCAL_RPC_PORT:9933 -p $LOCAL_WS_PORT:9944"
+
+    if (( $idx <= 0 ))
+    then
+        launch_parachain "$LOCAL_CONTAINER_NAME" "$DOCKER_EXTRA_PARAMS" "--bootnodes=$PARACHAIN_FIRST_BOOTNODE_ADDR --rpc-cors=all --rpc-external --ws-external"
+    else
+        launch_parachain "$LOCAL_CONTAINER_NAME" "$DOCKER_EXTRA_PARAMS" "--collator --rpc-cors=all --rpc-methods=Unsafe --unsafe-rpc-external"
+        sleep 10
+        DATA='{ "id":1, "jsonrpc":"2.0", "method":"author_insertKey", "params":["nmbs", "'"$nimbus_seed"'", "'"$nimbus_pk"'"] }'
+        curl -H 'Content-Type: application/json' --data "$DATA" localhost:$LOCAL_RPC_PORT
+        delete_container "$LOCAL_CONTAINER_NAME"
+
+        if (( $idx == 1 ))
+        then
+            launch_parachain "$LOCAL_CONTAINER_NAME" "$DOCKER_EXTRA_PARAMS" "--collator --node-key=$PARACHAIN_FIRST_BOOTNODE_NODE_KEY"
+        else
+            launch_parachain "$LOCAL_CONTAINER_NAME" "$DOCKER_EXTRA_PARAMS" "--bootnodes=$PARACHAIN_FIRST_BOOTNODE_ADDR --collator"
+        fi
+    fi  
+}
+
+launch_configured_parachain 0 "" ""
+launch_configured_parachain 1 "$PARACHAIN_NIMBUS_SEED" "$PARACHAIN_NIMBUS_PK"
