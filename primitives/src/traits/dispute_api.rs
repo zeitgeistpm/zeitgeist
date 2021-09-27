1093cc8cacda3aef9c504c659451be836f7e862f
Caio <c410.f3r@gmail.com>
2021-09-27 17:23:31 -0300

    Add a few unit tests to enhance robustness

--- primitives/src/traits/dispute_api.rs
+++ primitives/src/traits/dispute_api.rs
@@ -15,6 +15,7 @@ pub trait DisputeApi {
     fn on_dispute(
         disputes: &[MarketDispute<Self::AccountId, Self::BlockNumber>],
         market_id: &Self::MarketId,
+        market: &Market<Self::AccountId, Self::BlockNumber, Self::Moment>,
     ) -> DispatchResult;
 
     /// Manages markets resolutions moving all reported markets to resolved.
