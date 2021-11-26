704d67fa1e016502b49f32ad04c076d8da97f308
Harald Heckmann <mail@haraldheckmann.de>
2021-11-26 15:24:34 +0000

    Adjust orml_currencies benchmark logic & add custom weights

--- runtime/src/benchmarking/mod.rs
+++ runtime/src/benchmarking/mod.rs
@@ -1,6 +1,6 @@
 #![cfg(feature = "runtime-benchmarks")]
 #![allow(clippy::integer_arithmetic, clippy::indexing_slicing)]
 
-// pub mod currencies;
+pub mod currencies;
 pub mod tokens;
 pub mod utils;
