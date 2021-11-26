0343d7f4f6077d3456fa22897762e2b14b89a919
Harald Heckmann <mail@haraldheckmann.de>
2021-11-26 23:31:18 +0000

    Add most custom reference machine weights

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -1,3 +1,21 @@
+cfg_if::cfg_if!{
+    if #[cfg(feature = "parachain")] {
+        pub mod pallet_author_mapping;
+        pub mod parachain_staking;
+    } else {
+        pub mod pallet_grandpa;
+    }
+}
+
+
+pub mod frame_system;
 pub mod orml_currencies;
 pub mod orml_tokens;
 pub mod pallet_balances;
+pub mod pallet_collective;
+pub mod pallet_identity;
+pub mod pallet_membership;
+pub mod pallet_timestamp;
+pub mod pallet_treasury;
+pub mod pallet_utility;
+pub mod pallet_vesting;
