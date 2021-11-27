b250fb553497a844cfea2359b9995eecc8f6443c
Harald Heckmann <mail@haraldheckmann.de>
2021-11-27 14:03:15 +0000

    Remove pallet_crowdloan_rewards from benchmarks
    The benchmarking logic leads to a result which does not pass the test in the verification logic (benchmarks deprecated)

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -2,6 +2,8 @@ cfg_if::cfg_if! {
     if #[cfg(feature = "parachain")] {
         pub mod pallet_author_mapping;
         pub mod parachain_staking;
+        // Currently the benchmark does fail at the verification at least one function (deprecated)
+        // pub mod pallet_crowdloan_rewards
     } else {
         // Currently the benchmark does yield an invalid weight implementation
         // pub mod pallet_grandpa;
