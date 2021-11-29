10a9e05e003b965e812bb37549bff502b7726d6b
Harald Heckmann <mail@haraldheckmann.de>
2021-11-29 23:35:59 +0100

    Add reference machine weights (#386)
    * Implement benchmark script
    
    * Fix Zeitgeist benchmark execution
    
    * Make benchmark script stop on error
    
    * Add most custom reference machine weights
    
    * Cargo fmt
    
    * Remove custom grandpa weights
    
    The current benchmarking logic yields an invalid weight implementation.
    
    * Fix weight compilation issue for parachain pallets
    
    * Make frame weight template work for parachain pallets
    
    * Remove pallet_crowdloan_rewards from benchmarks
    
    The benchmarking logic leads to a result which does not pass the test in the verification logic (benchmarks deprecated)
    
    * Add final benchmark results
    
    * Cargo fmt
    
    * Satisfy Clippy

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -1,3 +1,23 @@
+cfg_if::cfg_if! {
+    if #[cfg(feature = "parachain")] {
+        pub mod pallet_author_mapping;
+        pub mod parachain_staking;
+        // Currently the benchmark does fail at the verification at least one function (deprecated)
+        // pub mod pallet_crowdloan_rewards
+    } else {
+        // Currently the benchmark does yield an invalid weight implementation
+        // pub mod pallet_grandpa;
+    }
+}
+
+pub mod frame_system;
 pub mod orml_currencies;
 pub mod orml_tokens;
 pub mod pallet_balances;
+pub mod pallet_collective;
+pub mod pallet_identity;
+pub mod pallet_membership;
+pub mod pallet_timestamp;
+pub mod pallet_treasury;
+pub mod pallet_utility;
+pub mod pallet_vesting;
