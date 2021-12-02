6ba0c8b8dd1f5f257585763e96ab1d54fad32d6a
Harald Heckmann <mail@haraldheckmann.de>
2021-12-02 18:14:08 +0000

    Set zeitgeist/dev as default for parachain/standalone

--- node/src/cli/cli_parachain.rs
+++ node/src/cli/cli_parachain.rs
@@ -26,7 +26,10 @@ impl RelayChainCli {
     ) -> Self {
         let extension = crate::chain_spec::Extensions::try_get(&*para_config.chain_spec);
         let chain_id = extension.map(|e| e.relay_chain.clone());
-        let base_path = para_config.base_path.as_ref().map(|x| x.path().join(chain_id.clone().unwrap_or("polkadot".into())));
+        let base_path = para_config
+            .base_path
+            .as_ref()
+            .map(|x| x.path().join(chain_id.clone().unwrap_or("polkadot".into())));
         Self { base_path, chain_id, base: polkadot_cli::RunCmd::from_iter(relay_chain_args) }
     }
 }
