9a772182ae9f700f83108e5a7a4f40af8f5d3366
Harald Heckmann <mail@haraldheckmann.de>
2021-12-08 14:04:24 +0000

    Update rust toolchain

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,8 @@ set -e
 echo "*** Initializing WASM build environment"
 
 if [ -z $CI_PROJECT_NAME ] ; then
-   rustup update nightly-2021-09-28
+   rustup update nightly-2021-12-08
    rustup update stable
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2021-09-28
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-12-08
