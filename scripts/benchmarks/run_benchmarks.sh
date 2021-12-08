4e7ebb0b77b364eacdccb696660b96c18b290d66
Harald Heckmann <mail@haraldheckmann.de>
2021-12-08 21:43:19 +0000

    add "--bin zeitgeist" to cargo build commands

--- scripts/benchmarks/run_benchmarks.sh
+++ scripts/benchmarks/run_benchmarks.sh
@@ -16,7 +16,7 @@ source ./scripts/benchmarks/configuration.sh
 
 # Standalone benchmarks
 
-cargo build --release --features=runtime-benchmarks
+cargo build --release --features=runtime-benchmarks --bin=zeitgeist
 
 
 for pallet in ${FRAME_PALLETS[@]}; do
@@ -36,7 +36,7 @@ done
 
 # Parachain benchmarks
 
-cargo build --release --features=parachain,runtime-benchmarks
+cargo build --release --features=parachain,runtime-benchmarks --bin=zeitgeist
 
 for pallet in ${FRAME_PALLETS_PARACHAIN[@]}; do
     ./target/release/zeitgeist benchmark --chain=dev --steps=50 --repeat=20 --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/frame_weight_template.hbs --output=./runtime/src/weights/
