7ab7e842233af8a8d44aeb9087b3345c1e8928c7
Harald Heckmann <mail@haraldheckmann.de>
2021-12-13 19:22:12 +0000

    Fix benchmark workflow error during error handling

--- scripts/benchmarks/run_benchmarks.sh
+++ scripts/benchmarks/run_benchmarks.sh
@@ -20,17 +20,17 @@ cargo build --release --features=runtime-benchmarks --bin=zeitgeist
 
 
 for pallet in ${FRAME_PALLETS[@]}; do
-    ./target/release/zeitgeist benchmark --chain=dev --steps=50 --repeat=20 --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/frame_weight_template.hbs --output=./runtime/src/weights/
+    ./target/release/zeitgeist benchmark --chain=dev --steps=$FRAME_PALLETS_STEPS --repeat=$FRAME_PALLETS_RUNS --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/frame_weight_template.hbs --output=./runtime/src/weights/
 done
 
 for pallet in ${ORML_PALLETS[@]}; do
-    ./target/release/zeitgeist benchmark --chain=dev --steps=50 --repeat=20 --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/orml_weight_template.hbs --output=./runtime/src/weights/
+    ./target/release/zeitgeist benchmark --chain=dev --steps=$ORML_PALLETS_STEPS --repeat=$ORML_PALLETS_RUNS --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/orml_weight_template.hbs --output=./runtime/src/weights/
 done
 
 for pallet in ${ZEITGEIST_PALLETS[@]}; do
     pallet_folder_name=${pallet//zrml_//}
     pallet_folder_name=${pallet_folder_name//_/-}
-    ./target/release/zeitgeist benchmark --chain=dev --steps=10 --repeat=1000 --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/weight_template.hbs --output=./zrml/$pallet_folder_name/src/weights.rs
+    ./target/release/zeitgeist benchmark --chain=dev --steps=$ZEITGEIST_PALLETS_STEPS --repeat=$ZEITGEIST_PALLETS_RUNS --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/weight_template.hbs --output=./zrml/$pallet_folder_name/src/weights.rs
 done
 
 
@@ -39,5 +39,5 @@ done
 cargo build --release --features=parachain,runtime-benchmarks --bin=zeitgeist
 
 for pallet in ${FRAME_PALLETS_PARACHAIN[@]}; do
-    ./target/release/zeitgeist benchmark --chain=dev --steps=50 --repeat=20 --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/frame_weight_template.hbs --output=./runtime/src/weights/
+    ./target/release/zeitgeist benchmark --chain=dev --steps=$FRAME_PALLETS_PARACHAIN_STEPS --repeat=$FRAME_PALLETS_PARACHAIN_RUNS --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/frame_weight_template.hbs --output=./runtime/src/weights/
 done
