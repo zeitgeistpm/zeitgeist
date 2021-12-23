7935dcddf9dc7ebc6305ad82f4e4c808e240abcc
Harald Heckmann <mail@haraldheckmann.de>
2021-12-23 22:57:14 +0100

    Add state changes to PM events (#415)
    * Add state changes to PM events
    
    * Remove redundant creator id from MarketCreated event
    
    * Set Report and Dispute types correctly in events
    
    Co-authored-by: sea212 <sea212@users.noreply.github.com>

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -65,7 +65,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("zeitgeist"),
     impl_name: create_runtime_str!("zeitgeist"),
     authoring_version: 1,
-    spec_version: 28,
+    spec_version: 29,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 8,
