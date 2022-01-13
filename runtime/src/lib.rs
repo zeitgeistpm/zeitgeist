e72126d2ea7e262d72e63fce41bc0e2d4ef23459
Harald Heckmann <mail@haraldheckmann.de>
2022-01-13 15:14:32 +0100

    Bump RuntimeVersion and add items to std feature

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -65,10 +65,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("zeitgeist"),
     impl_name: create_runtime_str!("zeitgeist"),
     authoring_version: 1,
-    spec_version: 30,
+    spec_version: 31,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 8,
+    transaction_version: 9,
 };
 
 pub type Block = generic::Block<Header, UncheckedExtrinsic>;
