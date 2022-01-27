9426ae93a5bd5481a445d703aa6a767d9e6b7a6a
Harald Heckmann <mail@haraldheckmann.de>
2022-01-27 16:34:06 +0100

    Bump RuntimeVersion

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -65,10 +65,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("zeitgeist"),
     impl_name: create_runtime_str!("zeitgeist"),
     authoring_version: 1,
-    spec_version: 31,
+    spec_version: 32,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 9,
+    transaction_version: 10,
 };
 
 pub type Block = generic::Block<Header, UncheckedExtrinsic>;
