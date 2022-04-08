1ccefb8eba8cdd7c9a2a2235dca8df832a8aadd3
maltekliemann <mail@maltekliemann.com>
2022-04-08 17:56:17 +0200

    Bump `spec_version`

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -58,7 +58,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("zeitgeist"),
     impl_name: create_runtime_str!("zeitgeist"),
     authoring_version: 1,
-    spec_version: 34,
+    spec_version: 35,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 11,
