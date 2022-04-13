56ae6539fece53f1aa6e7e3e62ffee6b92b1f822
maltekliemann <mail@maltekliemann.com>
2022-04-13 23:05:19 +0200

    Cleanup unnecessary changes

--- zrml/swaps/src/lib.rs
+++ zrml/swaps/src/lib.rs
@@ -1143,8 +1143,7 @@ mod pallet {
             amount: BalanceOf<T>,
         ) -> DispatchResult {
             let shares_id = Self::pool_shares_id(pool_id);
-            T::Shares::deposit(shares_id, to, amount)?;
-            Ok(())
+            T::Shares::deposit(shares_id, to, amount)
         }
 
         pub(crate) fn pool_shares_id(pool_id: PoolId) -> Asset<T::MarketId> {
