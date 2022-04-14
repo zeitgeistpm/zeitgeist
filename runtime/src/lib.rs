e356adccac4bd0183d528d82d1d1d5d4d1703c88
Harald Heckmann <mail@haraldheckmann.de>
2022-04-14 18:00:03 +0200

    Add MultiSig benchmark & weights

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -693,7 +693,7 @@ impl pallet_multisig::Config for Runtime {
 	type DepositBase = DepositBase;
 	type DepositFactor = DepositFactor;
 	type MaxSignatories = ConstU16<100>;
-	type WeightInfo = pallet_multisig::weights::SubstrateWeight<Runtime>;
+	type WeightInfo = weights::pallet_multisig::WeightInfo<Runtime>;
 }
 
 impl pallet_preimage::Config for Runtime {
@@ -981,6 +981,7 @@ impl_runtime_apis! {
             list_benchmark!(list, extra, pallet_democracy, Democracy);
             list_benchmark!(list, extra, pallet_identity, Identity);
             list_benchmark!(list, extra, pallet_membership, AdvisoryCommitteeMembership);
+            list_benchmark!(list, extra, pallet_multisig, MultiSig);
             list_benchmark!(list, extra, pallet_preimage, Preimage);
             list_benchmark!(list, extra, pallet_scheduler, Scheduler);
             list_benchmark!(list, extra, pallet_timestamp, Timestamp);
@@ -1051,6 +1052,7 @@ impl_runtime_apis! {
             add_benchmark!(params, batches, pallet_democracy, Democracy);
             add_benchmark!(params, batches, pallet_identity, Identity);
             add_benchmark!(params, batches, pallet_membership, AdvisoryCommitteeMembership);
+            add_benchmark!(params, batches, pallet_multisig, MultiSig);
             add_benchmark!(params, batches, pallet_preimage, Preimage);
             add_benchmark!(params, batches, pallet_scheduler, Scheduler);
             add_benchmark!(params, batches, pallet_timestamp, Timestamp);
--- scripts/benchmarks/configuration.sh
+++ scripts/benchmarks/configuration.sh
@@ -3,8 +3,9 @@
 # This script contains the configuration for other benchmarking scripts.
 
 export FRAME_PALLETS=( frame_system pallet_balances pallet_collective pallet_democracy \
-                pallet_identity pallet_membership  pallet_preimage pallet_scheduler \
-                pallet_timestamp pallet_treasury pallet_utility pallet_vesting ) # pallet_grandpa )
+                pallet_identity pallet_membership  pallet_multisig pallet_preimage \
+                pallet_scheduler pallet_timestamp pallet_treasury pallet_utility pallet_vesting \
+                ) # pallet_grandpa )
 export FRAME_PALLETS_RUNS=20
 export FRAME_PALLETS_STEPS=50
 
