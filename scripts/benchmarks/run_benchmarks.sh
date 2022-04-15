92b78bfafda334d3e986b641486c41bfa2518d2f
Harald Heckmann <mail@haraldheckmann.de>
2022-04-15 18:53:32 +0200

    Add "cargo fmt" after weight files are generated

--- scripts/benchmarks/run_benchmarks.sh
+++ scripts/benchmarks/run_benchmarks.sh
@@ -23,9 +23,13 @@ for pallet in ${FRAME_PALLETS[@]}; do
     ./target/release/zeitgeist benchmark --chain=dev --steps=$FRAME_PALLETS_STEPS --repeat=$FRAME_PALLETS_RUNS --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/frame_weight_template.hbs --output=./runtime/src/weights/
 done
 
+cargo fmt
+
 for pallet in ${ORML_PALLETS[@]}; do
     ./target/release/zeitgeist benchmark --chain=dev --steps=$ORML_PALLETS_STEPS --repeat=$ORML_PALLETS_RUNS --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/orml_weight_template.hbs --output=./runtime/src/weights/
 done
+
+cargo fmt
 
 for pallet in ${ZEITGEIST_PALLETS[@]}; do
     pallet_folder_name=${pallet//zrml_/}
@@ -33,6 +37,7 @@ for pallet in ${ZEITGEIST_PALLETS[@]}; do
     ./target/release/zeitgeist benchmark --chain=dev --steps=$ZEITGEIST_PALLETS_STEPS --repeat=$ZEITGEIST_PALLETS_RUNS --pallet=$pallet --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/weight_template.hbs --output=./zrml/$pallet_folder_name/src/weights.rs
 done
 
+cargo fmt
 
 # Parachain benchmarks
 
@@ -41,3 +46,5 @@ cargo build --release --features=parachain,runtime-benchmarks --bin=zeitgeist
 for pallet in ${FRAME_PALLETS_PARACHAIN[@]}; do
     ./target/release/zeitgeist benchmark --chain=dev --steps=$FRAME_PALLETS_PARACHAIN_STEPS --repeat=$FRAME_PALLETS_PARACHAIN_RUNS --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/frame_weight_template.hbs --output=./runtime/src/weights/
 done
+
+cargo fmt
