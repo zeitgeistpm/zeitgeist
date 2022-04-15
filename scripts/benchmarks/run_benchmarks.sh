007c54d4e3cb71cb97a3cd5c15387233f7e860d7
Harald Heckmann <mail@haraldheckmann.de>
2022-04-15 20:05:31 +0200

    Only cargo fmt once at the end

--- scripts/benchmarks/run_benchmarks.sh
+++ scripts/benchmarks/run_benchmarks.sh
@@ -18,26 +18,19 @@ source ./scripts/benchmarks/configuration.sh
 
 cargo build --release --features=runtime-benchmarks --bin=zeitgeist
 
-
 for pallet in ${FRAME_PALLETS[@]}; do
     ./target/release/zeitgeist benchmark --chain=dev --steps=$FRAME_PALLETS_STEPS --repeat=$FRAME_PALLETS_RUNS --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/frame_weight_template.hbs --output=./runtime/src/weights/
 done
 
-cargo fmt
-
 for pallet in ${ORML_PALLETS[@]}; do
     ./target/release/zeitgeist benchmark --chain=dev --steps=$ORML_PALLETS_STEPS --repeat=$ORML_PALLETS_RUNS --pallet=$pallet --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/orml_weight_template.hbs --output=./runtime/src/weights/
 done
-
-cargo fmt
 
 for pallet in ${ZEITGEIST_PALLETS[@]}; do
     pallet_folder_name=${pallet//zrml_/}
     pallet_folder_name=${pallet_folder_name//_/-}
     ./target/release/zeitgeist benchmark --chain=dev --steps=$ZEITGEIST_PALLETS_STEPS --repeat=$ZEITGEIST_PALLETS_RUNS --pallet=$pallet --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/weight_template.hbs --output=./zrml/$pallet_folder_name/src/weights.rs
 done
-
-cargo fmt
 
 # Parachain benchmarks
 
