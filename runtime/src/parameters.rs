63e6052a9e9e6972bb6834cfbee6e6a606adab86
Chralt98 <chralt98@gmail.com>
2022-04-26 10:05:53 +0200

    added comments for TargetedFeeAdjustment

--- runtime/src/parameters.rs
+++ runtime/src/parameters.rs
@@ -119,8 +119,22 @@ parameter_types! {
     // Transaction payment
     pub const OperationalFeeMultiplier: u8 = 5;
     pub const TransactionByteFee: Balance = 100 * MICRO;
-    pub TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
+    // The portion of the `NORMAL_DISPATCH_RATIO` that we adjust the fees with. Blocks filled less
+    // than this will decrease the weight and more will increase.
+    pub const TargetBlockFullness: Perquintill = Perquintill::from_percent(25);
+    // * https://paritytech.github.io/substrate/master/pallet_transaction_payment/struct.TargetedFeeAdjustment.html
+    // * With a target block time of 12 seconds (7200 blocks per day) 
+    // * where p is the amount of change over 7200 blocks.
+    // * p >= AdjustmentVariable * BlocksPerDay * (TargetBlockFullness * NORMAL_DISPATCH_RATIO)
+    // * p >= 0.00003 * 7200 * (1 - 0.25 * 0.75)
+    // * p >= 0.1755
+    // * Meaning that fees can change by around ~17.55% per day, given extreme congestion.
+    // The adjustment variable of the runtime. Higher values will cause `TargetBlockFullness` to
+    // change the fees more rapidly.
     pub AdjustmentVariable: Multiplier = Multiplier::saturating_from_rational(3, 100_000);
+    // Minimum amount of the multiplier. This value cannot be too low. A test case should ensure
+    // that combined with `AdjustmentVariable`, we can recover from the minimum.
+    // See `multiplier_can_grow_from_zero`.
     pub MinimumMultiplier: Multiplier = Multiplier::saturating_from_rational(1, 1_000_000u128);
 
     // Treasury
