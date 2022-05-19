d714a71de5a7c382bd99007f9650a34433c4d616
Chralt98 <chralt98@gmail.com>
2022-05-19 04:33:08 +0200

    fix clippy and rust errors

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1342,10 +1342,20 @@ impl_runtime_apis! {
 
     #[cfg(feature = "try-runtime")]
     impl frame_try_runtime::TryRuntime<Block> for Runtime {
-        fn on_runtime_upgrade() -> Result<(Weight, Weight), sp_runtime::RuntimeString> {
+        fn on_runtime_upgrade() -> (frame_support::weights::Weight, frame_support::weights::Weight) {
             log::info!("try-runtime::on_runtime_upgrade.");
-            let weight = Executive::try_runtime_upgrade()?;
-            Ok((weight, BlockWeights::get().max_block))
+            // NOTE: intentional unwrap: we don't want to propagate the error backwards, and want to
+            // have a backtrace here. If any of the pre/post migration checks fail, we shall stop
+            // right here and right now.
+            let weight = Executive::try_runtime_upgrade().map_err(|err|{
+                log::info!("try-runtime::on_runtime_upgrade failed with: {:?}", err);
+                err
+            }).unwrap();
+            (weight, RuntimeBlockWeights::get().max_block)
+        }
+
+        fn execute_block_no_check(block: Block) -> frame_support::weights::Weight {
+            Executive::execute_block_no_check(block)
         }
     }
 }
