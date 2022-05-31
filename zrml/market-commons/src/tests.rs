283ce7d80fafb0039ab352294fe979b01c865054
maltekliemann <mail@maltekliemann.com>
2022-05-31 13:53:53 +0200

    Make usage of oracle as sentinel more clear

--- zrml/market-commons/src/tests.rs
+++ zrml/market-commons/src/tests.rs
@@ -7,7 +7,25 @@ use crate::{
 };
 use frame_support::{assert_err, assert_noop, assert_ok};
 use sp_runtime::DispatchError;
-use zeitgeist_primitives::types::{AccountIdTest, BlockNumber, Market, Moment};
+use zeitgeist_primitives::types::{
+    AccountIdTest, BlockNumber, Market, MarketCreation, MarketDisputeMechanism, MarketPeriod,
+    MarketStatus, MarketType, Moment, ScoringRule,
+};
+
+const MARKET_DUMMY: Market<AccountIdTest, BlockNumber, Moment> = Market {
+    creation: MarketCreation::Permissionless,
+    creator_fee: 0,
+    creator: 0,
+    market_type: MarketType::Scalar(0..=100),
+    mdm: MarketDisputeMechanism::Authorized(0),
+    metadata: vec![],
+    oracle: 0,
+    period: MarketPeriod::Block(0..100),
+    report: None,
+    resolved_outcome: None,
+    scoring_rule: ScoringRule::CPMM,
+    status: MarketStatus::Disputed,
+};
 
 #[test]
 fn latest_market_id_interacts_correctly_with_push_market() {
@@ -59,9 +77,9 @@ fn markets_interacts_correctly_with_push_market() {
 fn market_fails_if_market_does_not_exist() {
     ExtBuilder::default().build().execute_with(|| {
         assert_noop!(MarketCommons::market(&0), crate::Error::<Runtime>::MarketDoesNotExist);
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
-        assert_ok!(MarketCommons::push_market(market_mock(1)));
-        assert_ok!(MarketCommons::push_market(market_mock(2)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_noop!(MarketCommons::market(&3), crate::Error::<Runtime>::MarketDoesNotExist);
     });
 }
@@ -85,9 +103,9 @@ fn mutate_market_fails_if_market_does_not_exist() {
             MarketCommons::mutate_market(&0, |_| Ok(())),
             crate::Error::<Runtime>::MarketDoesNotExist
         );
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
-        assert_ok!(MarketCommons::push_market(market_mock(1)));
-        assert_ok!(MarketCommons::push_market(market_mock(2)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_noop!(
             MarketCommons::mutate_market(&3, |_| Ok(())),
             crate::Error::<Runtime>::MarketDoesNotExist
@@ -139,9 +157,9 @@ fn remove_market_correctly_interacts_with_push_market() {
 fn remove_market_fails_if_market_does_not_exist() {
     ExtBuilder::default().build().execute_with(|| {
         assert_noop!(MarketCommons::remove_market(&0), crate::Error::<Runtime>::MarketDoesNotExist);
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
-        assert_ok!(MarketCommons::push_market(market_mock(1)));
-        assert_ok!(MarketCommons::push_market(market_mock(2)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_noop!(MarketCommons::remove_market(&3), crate::Error::<Runtime>::MarketDoesNotExist);
     });
 }
@@ -153,9 +171,9 @@ fn insert_market_pool_fails_if_market_does_not_exist() {
             MarketCommons::insert_market_pool(0, 15),
             crate::Error::<Runtime>::MarketDoesNotExist
         );
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
-        assert_ok!(MarketCommons::push_market(market_mock(1)));
-        assert_ok!(MarketCommons::push_market(market_mock(2)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_noop!(
             MarketCommons::insert_market_pool(3, 12),
             crate::Error::<Runtime>::MarketDoesNotExist
@@ -166,7 +184,7 @@ fn insert_market_pool_fails_if_market_does_not_exist() {
 #[test]
 fn insert_market_pool_fails_if_market_has_a_pool() {
     ExtBuilder::default().build().execute_with(|| {
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_ok!(MarketCommons::insert_market_pool(0, 15));
         assert_noop!(
             MarketCommons::insert_market_pool(0, 14),
@@ -178,9 +196,9 @@ fn insert_market_pool_fails_if_market_has_a_pool() {
 #[test]
 fn market_pool_correctly_interacts_with_insert_market_pool() {
     ExtBuilder::default().build().execute_with(|| {
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
-        assert_ok!(MarketCommons::push_market(market_mock(1)));
-        assert_ok!(MarketCommons::push_market(market_mock(2)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_ok!(MarketCommons::insert_market_pool(0, 15));
         assert_ok!(MarketCommons::insert_market_pool(1, 14));
         assert_ok!(MarketCommons::insert_market_pool(2, 13));
@@ -197,9 +215,9 @@ fn market_pool_fails_if_market_has_no_pool() {
             MarketCommons::market_pool(&0),
             crate::Error::<Runtime>::MarketPoolDoesNotExist
         );
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
-        assert_ok!(MarketCommons::push_market(market_mock(1)));
-        assert_ok!(MarketCommons::push_market(market_mock(2)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_ok!(MarketCommons::insert_market_pool(0, 15));
         assert_ok!(MarketCommons::insert_market_pool(1, 14));
         assert_ok!(MarketCommons::insert_market_pool(2, 13));
@@ -213,9 +231,9 @@ fn market_pool_fails_if_market_has_no_pool() {
 #[test]
 fn remove_market_pool_correctly_interacts_with_insert_market_pool() {
     ExtBuilder::default().build().execute_with(|| {
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
-        assert_ok!(MarketCommons::push_market(market_mock(1)));
-        assert_ok!(MarketCommons::push_market(market_mock(2)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_ok!(MarketCommons::insert_market_pool(0, 15));
         assert_ok!(MarketCommons::insert_market_pool(1, 14));
         assert_ok!(MarketCommons::insert_market_pool(2, 13));
@@ -262,9 +280,9 @@ fn remove_market_pool_fails_if_market_has_no_pool() {
             MarketCommons::remove_market_pool(&0),
             crate::Error::<Runtime>::MarketPoolDoesNotExist
         );
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
-        assert_ok!(MarketCommons::push_market(market_mock(1)));
-        assert_ok!(MarketCommons::push_market(market_mock(2)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_ok!(MarketCommons::insert_market_pool(0, 15));
         assert_ok!(MarketCommons::insert_market_pool(1, 14));
         assert_ok!(MarketCommons::insert_market_pool(2, 13));
@@ -279,17 +297,17 @@ fn remove_market_pool_fails_if_market_has_no_pool() {
 fn market_counter_interacts_correctly_with_push_market_and_remove_market() {
     ExtBuilder::default().build().execute_with(|| {
         assert_eq!(<MarketCounter<Runtime>>::get(), 0);
-        assert_ok!(MarketCommons::push_market(market_mock(0)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_eq!(<MarketCounter<Runtime>>::get(), 1);
-        assert_ok!(MarketCommons::push_market(market_mock(1)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_eq!(<MarketCounter<Runtime>>::get(), 2);
-        assert_ok!(MarketCommons::push_market(market_mock(2)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_eq!(<MarketCounter<Runtime>>::get(), 3);
         assert_ok!(MarketCommons::remove_market(&1));
         assert_eq!(<MarketCounter<Runtime>>::get(), 3);
         assert_ok!(MarketCommons::remove_market(&2));
         assert_eq!(<MarketCounter<Runtime>>::get(), 3);
-        assert_ok!(MarketCommons::push_market(market_mock(3)));
+        assert_ok!(MarketCommons::push_market(MARKET_DUMMY));
         assert_eq!(<MarketCounter<Runtime>>::get(), 4);
     });
 }
@@ -297,18 +315,7 @@ fn market_counter_interacts_correctly_with_push_market_and_remove_market() {
 fn market_mock(
     id: AccountIdTest,
 ) -> zeitgeist_primitives::types::Market<AccountIdTest, BlockNumber, Moment> {
-    Market {
-        creation: zeitgeist_primitives::types::MarketCreation::Permissionless,
-        creator_fee: 0,
-        creator: 0,
-        market_type: zeitgeist_primitives::types::MarketType::Scalar(0..=100),
-        mdm: zeitgeist_primitives::types::MarketDisputeMechanism::Authorized(0),
-        metadata: Default::default(),
-        oracle: id,
-        period: zeitgeist_primitives::types::MarketPeriod::Block(Default::default()),
-        report: None,
-        resolved_outcome: None,
-        scoring_rule: zeitgeist_primitives::types::ScoringRule::CPMM,
-        status: zeitgeist_primitives::types::MarketStatus::Disputed,
-    }
+    let mut market = MARKET_DUMMY;
+    market.oracle = id;
+    market
 }
