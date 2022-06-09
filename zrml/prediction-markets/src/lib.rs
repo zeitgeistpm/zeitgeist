5547fa91d149772e67dc3b4fbdef905772b45346
Chralt98 <chralt98@gmail.com>
2022-06-09 08:57:55 +0200

    improve state check in dispute

--- zrml/prediction-markets/src/lib.rs
+++ zrml/prediction-markets/src/lib.rs
@@ -349,7 +349,10 @@ mod pallet {
             let disputes = Disputes::<T>::get(market_id);
             let curr_block_num = <frame_system::Pallet<T>>::block_number();
             let market = T::MarketCommons::market(&market_id)?;
-            ensure!(market.status != MarketStatus::Resolved, Error::<T>::InvalidMarketStatus);
+            ensure!(
+                matches!(market.status, MarketStatus::Reported | MarketStatus::Disputed),
+                Error::<T>::InvalidMarketStatus
+            );
             let num_disputes: u32 = disputes.len().saturated_into();
             Self::validate_dispute(&disputes, &market, num_disputes, &outcome)?;
             CurrencyOf::<T>::reserve_named(
