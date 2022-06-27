67a4c9b28654879a87b97f514cf88982801ffaa7
Chralt98 <chralt98@gmail.com>
2022-06-27 10:42:14 +0200

    fix assertion for valid pool creation

--- zrml/swaps/fuzz/utils.rs
+++ zrml/swaps/fuzz/utils.rs
@@ -96,7 +96,7 @@ fn create_random_assets_and_weights(
     let mut assets: Vec<(u8, u128, u16)> = Vec::with_capacity(assets_len);
     let mut weights: Vec<u128> = Vec::with_capacity(assets_len);
 
-    assert!(MaxWeight::get() > MaxTotalWeight::get())
+    assert!(MaxWeight::get() <= MaxTotalWeight::get());
 
     let mut weight_sum = 0;
 
