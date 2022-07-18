789d565bde29cb58836dda658def7490b1163924
Vivek Pandya <vivekvpandya@gmail.com>
2022-07-18 09:15:45 +0530

    Use CountedStorageMap instead of StorageMap for Jurors in court pallet (#707)
    * Use CountedStorageMap instead of StorageMap for Jurors in court pallet
    
    * Add migration for Jurors in Court from StorageMap to CountedStorageMap
    
    * Remove storage migration for RemoveDisputesOfResolvedMarkets

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -77,7 +77,7 @@ type Executive = frame_executive::Executive<
     frame_system::ChainContext<Runtime>,
     Runtime,
     AllPalletsWithSystem,
-    zrml_prediction_markets::migrations::RemoveDisputesOfResolvedMarkets<Runtime>,
+    zrml_court::migrations::JurorsCountedStorageMapMigration<Runtime>,
 >;
 
 type Header = generic::Header<BlockNumber, BlakeTwo256>;
