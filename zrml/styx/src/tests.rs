e6a7f8919f492e9d39a9bb98836b3be61ab12a1a
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2022-07-31 22:47:13 +0200

    fmt

--- zrml/styx/src/tests.rs
+++ zrml/styx/src/tests.rs
@@ -10,8 +10,7 @@ fn should_emit_account_crossed_event_with_correct_value() {
         frame_system::Pallet::<Runtime>::set_block_number(1);
         assert_ok!(Styx::cross(Origin::signed(ALICE)));
         System::assert_last_event(
-            Event::AccountCrossed(ALICE, Asset::Ztg, crate::BurnAmount::<Runtime>::get())
-                .into(),
+            Event::AccountCrossed(ALICE, Asset::Ztg, crate::BurnAmount::<Runtime>::get()).into(),
         );
     });
 }
@@ -21,9 +20,7 @@ fn should_set_burn_amount() {
     ExtBuilder::default().build().execute_with(|| {
         frame_system::Pallet::<Runtime>::set_block_number(1);
         assert_ok!(Styx::set_burn_amount(Origin::signed(SUDO), 144u128));
-        System::assert_last_event(
-            Event::CrossingFeeChanged(SUDO, Asset::Ztg, 144u128).into(),
-        );
+        System::assert_last_event(Event::CrossingFeeChanged(SUDO, Asset::Ztg, 144u128).into());
         assert_eq!(crate::BurnAmount::<Runtime>::get(), 144u128);
     });
 }
