a6ec39273e5a20ecb38d79dea5b0374abe935b41
Harald Heckmann <mail@haraldheckmann.de>
2022-08-11 12:43:45 +0200

    Make the project capable of handling multiple runtimes (#721)
    * Migrate from modules to crates
    
    Previously all runtimes were united in one crate and toggled by using flags. The client must be able to offer all runtimes, which was impossible with all runtimes being in one crate: Rust prohibits linking to the same crate with different features. Consequently, each runtime must have its own crate. This complicates things significantly and if the previously common runtime schema wants to be used, wrapper macros have to be introduced.
    
    * Make client use required Executor and RuntimeApi
    
    * Adjust subcommand interpreter to select proper runtime
    
    * Add and implement abstract client
    
    * Make launcher select correct RuntimeAPI and Executor
    
    * Adjust SubstrateCli impl and make client work with chainspecs
    
    * Add macro to generate generic genesis across different runtimes

--- node/src/chain_spec/battery_station.rs
+++ node/src/chain_spec/battery_station.rs
@@ -1,37 +1,130 @@
-#[cfg(not(feature = "without-sudo"))]
-use crate::chain_spec::root_key_staging_testnet;
-use crate::chain_spec::{
-    additional_chain_spec_staging_testnet, endowed_accounts_staging_testnet, generic_genesis,
-    telemetry_endpoints, token_properties, zeitgeist_wasm, ChainSpec,
-};
+#![cfg(feature = "with-battery-station-runtime")]
+
+use super::{AdditionalChainSpec, EndowedAccountWithBalance};
+use crate::chain_spec::{generate_generic_genesis_function, telemetry_endpoints, token_properties};
+use battery_station_runtime::parameters::SS58Prefix;
+use hex_literal::hex;
 use sc_service::ChainType;
+use sp_core::crypto::UncheckedInto;
+use zeitgeist_primitives::{
+    constants::{
+        ztg::{LIQUIDITY_MINING, LIQUIDITY_MINING_PTD},
+        BASE,
+    },
+    types::AccountId,
+};
+
+#[cfg(feature = "parachain")]
+use {
+    super::{Extensions, DEFAULT_COLLATOR_INFLATION_INFO},
+    battery_station_runtime::{CollatorDeposit, EligibilityValue, PolkadotXcmConfig},
+};
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "parachain")] {
+        pub(super) const DEFAULT_STAKING_AMOUNT_BATTERY_STATION: u128 = 2_000 * BASE;
+        const DEFAULT_COLLATOR_BALANCE_BATTERY_STATION: Option<u128> =
+            DEFAULT_STAKING_AMOUNT_BATTERY_STATION.checked_add(CollatorDeposit::get());
+        const DEFAULT_INITIAL_CROWDLOAN_FUNDS_BATTERY_STATION: u128 = 100 * BASE;
+        pub type BatteryStationChainSpec = sc_service::GenericChainSpec<battery_station_runtime::GenesisConfig, Extensions>;
+    } else {
+        pub type BatteryStationChainSpec = sc_service::GenericChainSpec<battery_station_runtime::GenesisConfig>;
+    }
+}
+
+const DEFAULT_INITIAL_BALANCE_BATTERY_STATION: u128 = 10_000 * BASE;
+
+#[cfg(feature = "parachain")]
+fn additional_chain_spec_staging_battery_station(
+    parachain_id: cumulus_primitives_core::ParaId,
+) -> AdditionalChainSpec {
+    AdditionalChainSpec {
+        candidates: vec![(
+            hex!["302f6d7467ae2d7e3b9b962bfc3b9d929da9fae5f1e8c977a031ddf721b0790d"].into(),
+            hex!["e6ea0b63b2b5b7247a1e8280350a14c5f9e7745dec2fe3428b68aa4167d48e66"]
+                .unchecked_into(),
+            DEFAULT_STAKING_AMOUNT_BATTERY_STATION,
+        )],
+        crowdloan_fund_pot: DEFAULT_INITIAL_CROWDLOAN_FUNDS_BATTERY_STATION,
+        inflation_info: DEFAULT_COLLATOR_INFLATION_INFO,
+        nominations: vec![],
+        parachain_id,
+    }
+}
+
+#[cfg(not(feature = "parachain"))]
+fn additional_chain_spec_staging_battery_station() -> AdditionalChainSpec {
+    AdditionalChainSpec {
+        initial_authorities: vec![(
+            // 5FCSJzvmeUW1hBo3ASnLzSxpUdn5QUDt1Eqobj1meiQB7mLu
+            hex!["8a9a54bdf73fb4a757f5ab81fabe2f173922fdb92bb8b6e8bedf8b17fa38f500"]
+                .unchecked_into(),
+            // 5HGProUwcyCDMJDxjBBKbv8u7ehr5uoTBS3bckYHPcZMTifW
+            hex!["e61786c6426b55a034f9c4b78dc57d4183927cef8e64b2e496225ed6fca41758"]
+                .unchecked_into(),
+        )],
+    }
+}
+
+fn endowed_accounts_staging_battery_station() -> Vec<EndowedAccountWithBalance> {
+    vec![
+        // 5D2L4ghyiYE8p2z7VNJo9JYwRuc8uzPWtMBqdVyvjRcsnw4P
+        EndowedAccountWithBalance(
+            hex!["2a6c61a907556e4c673880b5767dd4be08339ee7f2a58d5137d0c19ca9570a5c"].into(),
+            DEFAULT_INITIAL_BALANCE_BATTERY_STATION,
+        ),
+        // 5EeeZVU4SiPG6ZRY7o8aDcav2p2mZMdu3ZLzbREWuHktYdhX
+        EndowedAccountWithBalance(
+            hex!["725bb6fd13d52b3d6830e5a9faed1f6499ca0f5e8aa285df09490646e71e831b"].into(),
+            DEFAULT_INITIAL_BALANCE_BATTERY_STATION,
+        ),
+        // 5D9tF8w1FMSdz52bpiaQis1pCUZy5Gs6HcHS7gHxEzyq4XzU
+        #[cfg(feature = "parachain")]
+        EndowedAccountWithBalance(
+            hex!["302f6d7467ae2d7e3b9b962bfc3b9d929da9fae5f1e8c977a031ddf721b0790d"].into(),
+            DEFAULT_COLLATOR_BALANCE_BATTERY_STATION.unwrap(),
+        ),
+    ]
+}
+
+fn root_key_staging_battery_station() -> AccountId {
+    hex!["2a6c61a907556e4c673880b5767dd4be08339ee7f2a58d5137d0c19ca9570a5c"].into()
+}
+
+#[inline]
+pub(super) fn get_wasm() -> Result<&'static [u8], String> {
+    battery_station_runtime::WASM_BINARY.ok_or_else(|| "WASM binary is not available".to_string())
+}
+
+generate_generic_genesis_function!(
+    battery_station_runtime,
+    sudo: battery_station_runtime::SudoConfig { key: Some(root_key_staging_battery_station()) },
+);
 
 pub fn battery_station_staging_config(
     #[cfg(feature = "parachain")] parachain_id: cumulus_primitives_core::ParaId,
-) -> Result<ChainSpec, String> {
-    let zeitgeist_wasm = zeitgeist_wasm()?;
+) -> Result<BatteryStationChainSpec, String> {
+    let wasm = get_wasm()?;
 
-    Ok(ChainSpec::from_genesis(
+    Ok(BatteryStationChainSpec::from_genesis(
         "Battery Station Staging",
         "battery_station_staging",
         ChainType::Live,
         move || {
             generic_genesis(
-                additional_chain_spec_staging_testnet(
+                additional_chain_spec_staging_battery_station(
                     #[cfg(feature = "parachain")]
                     parachain_id,
                 ),
-                endowed_accounts_staging_testnet(),
-                #[cfg(not(feature = "without-sudo"))]
-                root_key_staging_testnet(),
-                zeitgeist_wasm,
+                endowed_accounts_staging_battery_station(),
+                wasm,
             )
         },
         vec![],
         telemetry_endpoints(),
         Some("battery_station"),
         None,
-        Some(token_properties("ZBS")),
+        Some(token_properties("ZBS", SS58Prefix::get())),
         #[cfg(feature = "parachain")]
         crate::chain_spec::Extensions {
             relay_chain: "rococo".into(),
--- node/src/chain_spec/dev.rs
+++ node/src/chain_spec/dev.rs
@@ -1,21 +1,45 @@
-use super::EndowedAccountWithBalance;
-#[cfg(feature = "parachain")]
-use crate::chain_spec::get_from_seed;
-use crate::chain_spec::{
-    generic_genesis, get_account_id_from_seed, token_properties, zeitgeist_wasm,
-    AdditionalChainSpec, ChainSpec,
+#![cfg(feature = "with-battery-station-runtime")]
+
+use super::{
+    battery_station::BatteryStationChainSpec, generate_generic_genesis_function,
+    get_account_id_from_seed, get_from_seed, token_properties, AdditionalChainSpec,
+    EndowedAccountWithBalance,
 };
+
+#[cfg(feature = "parachain")]
+use battery_station_runtime::{EligibilityValue, PolkadotXcmConfig};
 use sc_service::ChainType;
 use sp_core::sr25519;
-use zeitgeist_primitives::types::Balance;
+use zeitgeist_primitives::{
+    constants::ztg::{LIQUIDITY_MINING, LIQUIDITY_MINING_PTD},
+    types::Balance,
+};
 
 const INITIAL_BALANCE: Balance = Balance::MAX >> 4;
 
+#[cfg(not(feature = "parachain"))]
+fn authority_keys_from_seed(
+    s: &str,
+) -> (sp_consensus_aura::sr25519::AuthorityId, sp_finality_grandpa::AuthorityId) {
+    (
+        get_from_seed::<sp_consensus_aura::sr25519::AuthorityId>(s),
+        get_from_seed::<sp_finality_grandpa::AuthorityId>(s),
+    )
+}
+
+generate_generic_genesis_function! {battery_station_runtime,
+    sudo: battery_station_runtime::SudoConfig { key: Some(get_account_id_from_seed::<sr25519::Public>("Alice")) },
+}
+
+// Dev currently uses battery station runtime for the following reasons:
+// 1. It is the most experimental runtime (as of writing this)
+// 2. It offers the sudo pallet
 pub fn dev_config(
     #[cfg(feature = "parachain")] parachain_id: cumulus_primitives_core::ParaId,
-) -> Result<ChainSpec, String> {
-    let zeitgeist_wasm = zeitgeist_wasm()?;
-    Ok(ChainSpec::from_genesis(
+) -> Result<BatteryStationChainSpec, String> {
+    let wasm = super::battery_station::get_wasm()?;
+
+    Ok(BatteryStationChainSpec::from_genesis(
         "Development",
         "dev",
         ChainType::Local,
@@ -26,7 +50,7 @@ pub fn dev_config(
                     candidates: vec![(
                         get_account_id_from_seed::<sr25519::Public>("Alice"),
                         get_from_seed::<nimbus_primitives::NimbusId>("Alice"),
-                        crate::chain_spec::DEFAULT_STAKING_AMOUNT_TESTNET,
+                        super::battery_station::DEFAULT_STAKING_AMOUNT_BATTERY_STATION,
                     )],
                     crowdloan_fund_pot: zeitgeist_primitives::constants::BASE.saturating_mul(100),
                     inflation_info: crate::chain_spec::DEFAULT_COLLATOR_INFLATION_INFO,
@@ -35,7 +59,7 @@ pub fn dev_config(
                 },
                 #[cfg(not(feature = "parachain"))]
                 AdditionalChainSpec {
-                    initial_authorities: vec![crate::chain_spec::authority_keys_from_seed("Alice")],
+                    initial_authorities: vec![authority_keys_from_seed("Alice")],
                 },
                 vec![
                     get_account_id_from_seed::<sr25519::Public>("Alice"),
@@ -54,16 +78,14 @@ pub fn dev_config(
                 .into_iter()
                 .map(|acc| EndowedAccountWithBalance(acc, INITIAL_BALANCE))
                 .collect(),
-                #[cfg(not(feature = "without-sudo"))]
-                get_account_id_from_seed::<sr25519::Public>("Alice"),
-                zeitgeist_wasm,
+                wasm,
             )
         },
         vec![],
         None,
         None,
         None,
-        Some(token_properties("DEV")),
+        Some(token_properties("DEV", battery_station_runtime::SS58Prefix::get())),
         #[cfg(feature = "parachain")]
         crate::chain_spec::Extensions {
             relay_chain: "rococo-dev".into(),
