6cea4e3feae5c7e7d6ee5d993aa0617a75162849
Chralt <chralt98@gmail.com>
2022-09-17 08:59:58 +0200

    Update zrml/global-disputes/src/benchmarks.rs
    Co-authored-by: Harald Heckmann <mail@haraldheckmann.de>

--- zrml/global-disputes/src/benchmarks.rs
+++ zrml/global-disputes/src/benchmarks.rs
@@ -36,7 +36,6 @@ use sp_runtime::traits::{Bounded, SaturatedConversion};
 use sp_std::prelude::*;
 use zeitgeist_primitives::types::OutcomeReport;
 
-// ./target/release/zeitgeist benchmark pallet --chain=dev --steps=20 --repeat=50 --pallet=zrml_global_disputes --extrinsic='*' --execution=wasm --wasm-execution=compiled --heap-pages=4096 --template=./misc/weight_template.hbs --output=./zrml/global-disputes/src/weights.rs
 
 fn deposit<T>(caller: &T::AccountId)
 where
