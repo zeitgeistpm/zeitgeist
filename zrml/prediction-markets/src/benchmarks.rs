19bf69c7f32cfd33209d9cb7d4d5366ed22454fb
Chralt <chralt.developer@gmail.com>
2022-10-05 12:02:54 +0200

    Update zrml/prediction-markets/src/benchmarks.rs
    Co-authored-by: Vivek Pandya <vivekvpandya@gmail.com>

--- zrml/prediction-markets/src/benchmarks.rs
+++ zrml/prediction-markets/src/benchmarks.rs
@@ -303,7 +303,7 @@ benchmarks! {
 
         let pool_id = T::MarketCommons::market_pool(&market_id)?;
 
-        let market = T::MarketCommons::market(&market_id.saturated_into())?;
+        let market = T::MarketCommons::market(&market_id)?;
 
         let (range_start, range_end) = match market.period {
             MarketPeriod::Timestamp(range) => (range.start, range.end),
