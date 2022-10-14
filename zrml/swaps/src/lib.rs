d693094c57aceaa2d6857e7e0028220df33b44e3
maltekliemann <mail@maltekliemann.com>
2022-10-14 15:50:52 +0200

    Change hasher of cache

--- zrml/swaps/src/lib.rs
+++ zrml/swaps/src/lib.rs
@@ -1086,7 +1086,7 @@ mod pallet {
 
     #[pallet::storage]
     #[pallet::getter(fn pools_cached_for_arbitrage)]
-    pub type PoolsCachedForArbitrage<T: Config> = StorageMap<_, Twox64Concat, PoolId, ()>;
+    pub type PoolsCachedForArbitrage<T: Config> = StorageMap<_, Blake2_128Concat, PoolId, ()>;
 
     #[pallet::storage]
     #[pallet::getter(fn subsidy_providers)]
