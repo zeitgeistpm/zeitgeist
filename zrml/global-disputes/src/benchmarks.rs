693990c5aa8a5caf1dc724a9f8a1ac7dd8d91d8c
Chralt <chralt.developer@gmail.com>
2022-11-03 13:36:50 +0100

    Update zrml/global-disputes/src/benchmarks.rs
    Co-authored-by: Malte Kliemann <mail@maltekliemann.com>

--- zrml/global-disputes/src/benchmarks.rs
+++ zrml/global-disputes/src/benchmarks.rs
@@ -1,6 +1,7 @@
 //! Global Disputes pallet benchmarking.
 
-// Copyright 2021-2022 Zeitgeist PM LLC.
+// Copyright 2022 Forecasting Technologies LTD.
+// Copyright 2022 Zeitgeist PM LLC.
 //
 // This file is part of Zeitgeist.
 //
