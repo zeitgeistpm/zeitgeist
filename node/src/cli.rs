96d57a0d64c32b6076a0bf272178327b84297bb5
maltekliemann <mail@maltekliemann.com>
2022-11-14 13:04:23 +0100

    Fix license file

--- node/src/cli.rs
+++ node/src/cli.rs
@@ -1,4 +1,5 @@
 // Copyright 2021-2022 Zeitgeist PM LLC.
+// Copyright 2017-2020 Parity Technologies (UK) Ltd.
 //
 // This file is part of Zeitgeist.
 //
@@ -14,25 +15,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Zeitgeist. If not, see <https://www.gnu.org/licenses/>.
-//
-// This file incorporates work covered by the following copyright and
-// permission notice:
-//
-//     Copyright 2017-2020 Parity Technologies (UK) Ltd.
-//     This file is part of Polkadot.
-//
-//     Polkadot is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-//
-//     Polkadot is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-//
-//     You should have received a copy of the GNU General Public License
-//     along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 #[cfg(feature = "parachain")]
 mod cli_parachain;
