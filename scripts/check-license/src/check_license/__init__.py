7927eb9e3ed39eb211e7b7ebebdbd254dcbc29f2
Malte Kliemann <mail@maltekliemann.com>
2023-02-03 16:03:02 +0100

    Allow empty list of files

--- scripts/check-license/src/check_license/__init__.py
+++ scripts/check-license/src/check_license/__init__.py
@@ -9,7 +9,7 @@ from check_license.check_license import check_files, update_files
 def main():
     # TODO Add option to ignore files?
     parser = argparse.ArgumentParser()
-    parser.add_argument("files", nargs="+")
+    parser.add_argument("files", nargs="*")
     parser.add_argument("-w", "--write", action="store_true")
     args = parser.parse_args(sys.argv[1:])
     current_year = datetime.date.today().year
