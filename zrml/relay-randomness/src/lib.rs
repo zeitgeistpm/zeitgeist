1f595219a38dabb6f64b1b49117522998889f211
Chralt98 <chralt98@gmail.com>
2023-03-07 13:56:38 +0100

    make on_validation_data infallible

--- zrml/relay-randomness/src/lib.rs
+++ zrml/relay-randomness/src/lib.rs
@@ -8,7 +8,10 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-use frame_support::traits::{Get, Randomness};
+use frame_support::{
+    log,
+    traits::{Get, Randomness},
+};
 use parity_scale_codec::Encode;
 use sp_runtime::traits::Hash;
 
@@ -62,18 +65,30 @@ pub struct CustomSystemEventHandler<T>(sp_std::marker::PhantomData<T>);
 
 impl<T: Config> cumulus_pallet_parachain_system::OnSystemEvent for CustomSystemEventHandler<T> {
     fn on_validation_data(data: &cumulus_primitives_core::PersistedValidationData) {
-        let _relay_parent_storage_root =
-            cumulus_pallet_parachain_system::Pallet::<T>::validation_data()
-                .expect("set in `set_validation_data`")
-                .relay_parent_storage_root;
-        let relay_chain_state = cumulus_pallet_parachain_system::Pallet::<T>::relay_state_proof()
-            .expect("set in `set_validation_data`");
-        let proof = cumulus_pallet_parachain_system::RelayChainStateProof::new(
+        let relay_chain_state = if let Some(relay_chain_state) =
+            cumulus_pallet_parachain_system::Pallet::<T>::relay_state_proof()
+        {
+            relay_chain_state
+        } else {
+            log::warn!(
+                "Relaychain Randomness: No relay chain state proof available (set in \
+                 `set_validation_data`)"
+            );
+            return;
+        };
+
+        let proof = if let Some(proof) = cumulus_pallet_parachain_system::RelayChainStateProof::new(
             <T as pallet::Config>::SelfParaId::get(),
             data.relay_parent_storage_root,
             relay_chain_state,
         )
-        .expect("Invalid relay chain proof");
+        .ok()
+        {
+            proof
+        } else {
+            log::warn!("Relaychain Randomness: Invalid relay chain proof.");
+            return;
+        };
 
         let block_number = <frame_system::Pallet<T>>::block_number();
 
@@ -96,6 +111,7 @@ impl<T: Config> cumulus_pallet_parachain_system::OnSystemEvent for CustomSystemE
             randomness_source.current_block_randomness = Some((r, block_number));
             is_changed = true;
         }
+
         let one_epoch_ago_randomness: Option<[u8; 32]> = proof
             .read_entry::<sp_consensus_vrf::schnorrkel::Randomness>(
                 relay_well_known_keys::ONE_EPOCH_AGO_RANDOMNESS,
@@ -106,6 +122,7 @@ impl<T: Config> cumulus_pallet_parachain_system::OnSystemEvent for CustomSystemE
             randomness_source.one_epoch_ago_randomness = Some((r, block_number));
             is_changed = true;
         }
+		
         let two_epochs_ago_randomness: Option<[u8; 32]> = proof
             .read_entry::<sp_consensus_vrf::schnorrkel::Randomness>(
                 relay_well_known_keys::TWO_EPOCHS_AGO_RANDOMNESS,
