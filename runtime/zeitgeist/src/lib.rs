1cc21e8fe634588fd85bddefdd7dc2018ae8213d
Malte Kliemann <mail@maltekliemann.com>
2023-03-12 20:40:44 +0100

    Remove unused imports

--- runtime/zeitgeist/src/lib.rs
+++ runtime/zeitgeist/src/lib.rs
@@ -112,14 +112,6 @@ impl Contains<Call> for IsCallable {
         use pallet_collective::Call::set_members;
         use pallet_vesting::Call::force_vested_transfer;
 
-        use zeitgeist_primitives::types::{
-            MarketDisputeMechanism::{Court, SimpleDisputes},
-            ScoringRule::RikiddoSigmoidFeeMarketEma,
-        };
-        use zrml_prediction_markets::Call::{
-            create_cpmm_market_and_deploy_assets, create_market, edit_market,
-        };
-
         #[allow(clippy::match_like_matches_macro)]
         match call {
             // Membership is managed by the respective Membership instance
