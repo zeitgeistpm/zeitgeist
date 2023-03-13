0d4cd830dd14c3c0e3f854862a65210817e17ceb
Harald Heckmann <mail@haraldheckmann.de>
2023-03-13 18:55:47 +0100

    Bump Battery Station `spec_version` in migration runtime (#990)
    * Add kusama migration runtime
    
    * Remove Tokens callfilter
    
    Nothing can be call-filtered, since the call enum for the tokens pallet is not exposed within the runtime
    
    * Remove unused imports
    
    * bump spec_version
    
    * Filter business logic
    
    * Remove unused imports
    
    * add feature parachain to staking (#5)
    
    * Remove Client changes
    
    This is a runtime-only release. The proper client will be provided in the release that covers the runtime and client after the migration
    
    * empty commit for full CI
    
    * Disable previous migrations
    
    * Bump battery station spec_version
    
    * Update lib.rs
    
    ---------
    
    Co-authored-by: Chralt <chralt.developer@gmail.com>
    Co-authored-by: Malte Kliemann <mail@maltekliemann.com>
    Co-authored-by: Chralt98 <chralt98@gmail.com>

--- runtime/battery-station/src/lib.rs
+++ runtime/battery-station/src/lib.rs
@@ -91,10 +91,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("zeitgeist"),
     impl_name: create_runtime_str!("zeitgeist"),
     authoring_version: 1,
-    spec_version: 42,
+    spec_version: 44,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 19,
+    transaction_version: 20,
     state_version: 1,
 };
 
