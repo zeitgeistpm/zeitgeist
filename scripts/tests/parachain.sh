9f60969ba8aa9b6206345cb71ef557928195d742
Harald Heckmann <mail@haraldheckmann.de>
2023-05-25 16:21:13 +0200

    Use --all-features in the parachain case

--- scripts/tests/parachain.sh
+++ scripts/tests/parachain.sh
@@ -6,8 +6,8 @@ set -euxo pipefail
 
 . "$(dirname "$0")/aux-functions.sh" --source-only
 
-check_package_with_feature runtime/battery-station parachain,runtime-benchmarks,std,try-runtime
+check_package_with_feature runtime/battery-station 'parachain --all-features'
 
-check_package_with_feature runtime/zeitgeist parachain,runtime-benchmarks,std,try-runtime
+check_package_with_feature runtime/zeitgeist 'parachain --all-features'
 
-check_package_with_feature node default,parachain,runtime-benchmarks,try-runtime
+check_package_with_feature node 'parachain --all-features'
