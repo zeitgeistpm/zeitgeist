c5f6492b1f5a76799d5db423da873bf84907fb61
Harald Heckmann <mail@haraldheckmann.de>
2023-05-25 16:09:59 +0200

    Test all features in test scripts

--- scripts/tests/parachain.sh
+++ scripts/tests/parachain.sh
@@ -6,7 +6,8 @@ set -euxo pipefail
 
 . "$(dirname "$0")/aux-functions.sh" --source-only
 
-check_package_with_feature runtime/battery-station std,parachain
-check_package_with_feature runtime/zeitgeist std,parachain
+check_package_with_feature runtime/battery-station parachain,runtime-benchmarks,std,try-runtime
 
-check_package_with_feature node default,parachain
+check_package_with_feature runtime/zeitgeist parachain,runtime-benchmarks,std,try-runtime
+
+check_package_with_feature node default,parachain,runtime-benchmarks,try-runtime
--- scripts/tests/standalone.sh
+++ scripts/tests/standalone.sh
@@ -6,11 +6,8 @@ set -euxo pipefail
 
 . "$(dirname "$0")/aux-functions.sh" --source-only
 
-check_package_with_feature runtime/battery-station std
-check_package_with_feature runtime/battery-station std,runtime-benchmarks
+check_package_with_feature runtime/battery-station std,runtime-benchmarks,try-runtime
 
-check_package_with_feature runtime/zeitgeist std
-check_package_with_feature runtime/zeitgeist std,runtime-benchmarks
+check_package_with_feature runtime/zeitgeist std,runtime-benchmarks,try-runtime
 
-check_package_with_feature node default
-check_package_with_feature node default,runtime-benchmarks
+check_package_with_feature node default,runtime-benchmarks,try-runtime
