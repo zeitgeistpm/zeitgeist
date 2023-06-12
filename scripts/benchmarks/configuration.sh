d34145c111bc864200cb4566ec1eea9712ab2315
Harald Heckmann <mail@haraldheckmann.de>
2023-06-12 19:52:31 +0200

    Add pallet-contracts to benchmark script

--- scripts/benchmarks/configuration.sh
+++ scripts/benchmarks/configuration.sh
@@ -3,21 +3,18 @@
 EXTERNAL_WEIGHTS_PATH="./runtime/common/src/weights/"
 
 # This script contains the configuration for other benchmarking scripts.
-export FRAME_PALLETS=( frame_system pallet_balances pallet_bounties pallet_democracy \
-                pallet_identity pallet_membership  pallet_multisig pallet_preimage \
-                pallet_proxy pallet_scheduler pallet_timestamp pallet_treasury \
-                pallet_utility pallet_vesting pallet_collective ) # pallet_grandpa )
+export FRAME_PALLETS=( frame_system pallet_balances pallet_bounties pallet_collective \
+                        pallet_contracts pallet_democracy pallet_identity pallet_membership \
+                        pallet_multisig pallet_preimage pallet_proxy pallet_scheduler \
+                        pallet_timestamp pallet_treasury pallet_utility pallet_vesting \
+                    ) # pallet_grandpa )
 export FRAME_PALLETS_RUNS="${FRAME_PALLETS_RUNS:-20}"
 export FRAME_PALLETS_STEPS="${FRAME_PALLETS_STEPS:-50}"
 
 export FRAME_WEIGHT_TEMPLATE="./misc/frame_weight_template.hbs"
 
 export FRAME_PALLETS_PARACHAIN=( 
-                cumulus_pallet_xcmp_queue \
-                pallet_author_inherent \
-                pallet_author_slot_filter \
-                pallet_author_mapping \
-                pallet_parachain_staking \
+                cumulus_pallet_xcmp_queue pallet_author_inherent pallet_author_slot_filter pallet_author_mapping pallet_parachain_staking \
 )
 export FRAME_PALLETS_PARACHAIN_RUNS="${FRAME_PALLETS_PARACHAIN_RUNS:-$FRAME_PALLETS_RUNS}"
 export FRAME_PALLETS_PARACHAIN_STEPS="${FRAME_PALLETS_PARACHAIN_STEPS:-$FRAME_PALLETS_STEPS}"
