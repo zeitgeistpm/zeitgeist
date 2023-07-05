e2e4d21ef6374adf4ce4e3acc9794cfeed8759c6
Chralt98 <chralt98@gmail.com>
2023-07-05 07:48:23 +0200

    use FungiblesAdapter

--- runtime/common/src/fees.rs
+++ runtime/common/src/fees.rs
@@ -71,6 +71,7 @@ macro_rules! impl_foreign_fees {
         use frame_support::traits::fungibles::CreditOf;
         use frame_support::traits::tokens::fungibles::Inspect;
         use frame_support::traits::tokens::WithdrawReasons;
+        use frame_support::traits::tokens::BalanceConversion;
         use frame_support::traits::ExistenceRequirement;
         use frame_support::traits::tokens::WithdrawConsequence;
         use sp_runtime::traits::DispatchInfoOf;
@@ -79,31 +80,18 @@ macro_rules! impl_foreign_fees {
         use frame_support::traits::tokens::fungibles::Balanced;
         use zrml_swaps::check_arithm_rslt::CheckArithmRslt;
         use sp_runtime::traits::Convert;
+        use pallet_asset_tx_payment::HandleCredit;
 
         // It does foreign fees by extending transactions to include an optional `AssetId` that specifies the asset
         // to be used for payment (defaulting to the native token on `None`). So for each transaction you can specify asset id
         // For real ZTG you use None and for orml_tokens ZTG you use `Some(Asset::Ztg)` and for DOT you use `Some(Asset::Foreign(0))`
-        pub struct TokensTxCharger;
-
-        type TTCBalance = <TokensTxCharger as pallet_asset_tx_payment::OnChargeAssetTransaction<Runtime>>::Balance;
-        type TTCAsset = <TokensTxCharger as pallet_asset_tx_payment::OnChargeAssetTransaction<Runtime>>::AssetId;
-        type TTCLiquidityInfo = <TokensTxCharger as pallet_asset_tx_payment::OnChargeAssetTransaction<Runtime>>::LiquidityInfo;
 
         pub(crate) fn calculate_fee(
-            native_fee: TTCBalance,
-            fee_factor: TTCBalance,
-            base: TTCBalance,
-        ) -> Result<TTCBalance, TransactionValidityError> {
-            // We don't know the precision of the underlying asset. Because the converted fee could be
-            // less than one (e.g. 0.5) but gets rounded down by integer division we introduce a minimum
-            // fee.
-            let min_converted_fee: TTCBalance = if native_fee.is_zero() {
-                Zero::zero()
-            } else {
-                One::one()
-            };
-
-            let bmul = |a: TTCBalance, b: TTCBalance, base: TTCBalance| -> Option<TTCBalance> {
+            native_fee: Balance,
+            fee_factor: Balance,
+            base: Balance,
+        ) -> Result<Balance, TransactionValidityError> {
+            let bmul = |a: Balance, b: Balance, base: Balance| -> Option<Balance> {
                 let c0 = a.check_mul_rslt(&b).ok()?;
                 // The addition of base / 2 before the final division is a way to round to the nearest whole number
                 // if the fractional part of (a * b) / base is 0.5 or greater, it rounds up, otherwise, it rounds down.
@@ -118,30 +106,14 @@ macro_rules! impl_foreign_fees {
             // which is 143_120_520 / 10^10 = 0.0143120520 DOT
             let converted_fee = bmul(native_fee, fee_factor, base)
                 .ok_or(TransactionValidityError::Invalid(InvalidTransaction::Custom(0u8)))?;
-            let converted_fee = converted_fee.max(min_converted_fee);
 
             Ok(converted_fee)
         }
 
-        pub(crate) fn handle_withdraw(
-            asset_id: TTCAsset,
-            who: &AccountId,
-            converted_fee: TTCBalance,
-        ) -> Result<TTCLiquidityInfo, TransactionValidityError> {
-            let can_withdraw =
-                <Tokens as frame_support::traits::fungibles::Inspect<AccountId>>::can_withdraw(asset_id, who, converted_fee);
-            if !matches!(can_withdraw, WithdrawConsequence::Success) {
-                return Err(InvalidTransaction::Custom(9u8).into());
-            }
-
-            <Tokens as Balanced<AccountId>>::withdraw(asset_id, who, converted_fee)
-                .map_err(|_| TransactionValidityError::from(InvalidTransaction::Custom(1u8)))
-        }
-
         #[cfg(feature = "parachain")]
         pub(crate) fn get_fee_factor_and_base(
-            asset_id: TTCAsset,
-        ) -> Result<(TTCBalance, TTCBalance), TransactionValidityError> {
+            asset_id: CurrencyId,
+        ) -> Result<(Balance, Balance), TransactionValidityError> {
             let location = AssetConvert::convert(asset_id);
             let metadata = location.and_then(|loc| <AssetRegistry as orml_traits::asset_registry::Inspect>::metadata_by_location(&loc))
                 .ok_or(TransactionValidityError::Invalid(InvalidTransaction::Custom(2u8)))?;
@@ -152,97 +124,34 @@ macro_rules! impl_foreign_fees {
             Ok((fee_factor, base))
         }
 
-        impl pallet_asset_tx_payment::OnChargeAssetTransaction<Runtime> for TokensTxCharger {
-            type AssetId = CurrencyId;
-            type Balance = Balance;
-            type LiquidityInfo = CreditOf<AccountId, Tokens>;
+        pub struct TTCBalanceToAssetBalance;
+        impl BalanceConversion<Balance, CurrencyId, Balance> for TTCBalanceToAssetBalance {
+            type Error = TransactionValidityError;
 
-            fn withdraw_fee(
-                who: &AccountId,
-                call: &RuntimeCall,
-                _dispatch_info: &DispatchInfoOf<RuntimeCall>,
-                asset_id: Self::AssetId,
-                native_fee: Self::Balance,
-                _tip: Self::Balance,
-            ) -> Result<Self::LiquidityInfo, TransactionValidityError>
-            {
+            fn to_asset_balance(native_fee: Balance, asset_id: CurrencyId) -> Result<Balance, Self::Error> {
                 match asset_id {
-                    Asset::Ztg => {
-                        // this is the case that `Some(Asset::Ztg)` is used,
-                        // but we can't deal with pallet_balances here,
-                        // because LiquidityInfo is based on orml_tokens
-                        // so we use orml_tokens `Asset::Ztg`, which is not the real ZTG of pallet_balances
-                        // to get the real ZTG from pallet_balances, you need to specify `None` as asset_id for each transaction
-                        let min_converted_fee: TTCBalance = if native_fee.is_zero() {
-                            Zero::zero()
-                        } else {
-                            One::one()
-                        };
-                        let converted_fee = native_fee.max(min_converted_fee);
-                        handle_withdraw(Asset::Ztg, who, converted_fee)
-                    },
+                    Asset::Ztg => Ok(native_fee),
                     #[cfg(not(feature = "parachain"))]
-                    Asset::ForeignAsset(_) => return Err(TransactionValidityError::Invalid(InvalidTransaction::Custom(5u8))),
+                    Asset::ForeignAsset(_) => return Err(TransactionValidityError::Invalid(InvalidTransaction::Custom(1u8))),
                     #[cfg(feature = "parachain")]
                     Asset::ForeignAsset(_) => {
                         let (fee_factor, base) = get_fee_factor_and_base(asset_id)?;
                         let converted_fee = calculate_fee(native_fee, fee_factor, base)?;
-                        handle_withdraw(asset_id, who, converted_fee)
-                    },
+                        Ok(converted_fee)
+                    }
                     Asset::CategoricalOutcome(_, _) |
                     Asset::ScalarOutcome(_, _) |
                     Asset::CombinatorialOutcome |
-                    Asset::PoolShare(_) => return Err(TransactionValidityError::Invalid(InvalidTransaction::Custom(6u8))),
+                    Asset::PoolShare(_) => return Err(TransactionValidityError::Invalid(InvalidTransaction::Custom(2u8))),
                 }
             }
+        }
 
-            fn correct_and_deposit_fee(
-                who: &AccountId,
-                _dispatch_info: &DispatchInfoOf<RuntimeCall>,
-                _post_info: &PostDispatchInfoOf<RuntimeCall>,
-                corrected_native_fee: Self::Balance,
-                _tip: Self::Balance,
-                paid: Self::LiquidityInfo,
-            ) -> Result<(), TransactionValidityError> {
-                let asset_id = paid.asset();
-                let final_fee = match asset_id {
-                    Asset::Ztg => {
-                        // Calculate how much refund we should return.
-                        let (fee, refund) = paid.split(corrected_native_fee);
-
-                        // Refund to the account that paid the fees. If this fails, the account might have dropped
-                        // below the existential balance. In that case we don't refund anything.
-                        let _ = <Tokens as Balanced<AccountId>>::resolve(who, refund);
-
-                        fee
-                    },
-                    #[cfg(not(feature = "parachain"))]
-                    Asset::ForeignAsset(_) => return Err(TransactionValidityError::Invalid(InvalidTransaction::Custom(7u8))),
-                    #[cfg(feature = "parachain")]
-                    Asset::ForeignAsset(_) => {
-                        let (fee_factor, base) = get_fee_factor_and_base(asset_id)?;
-                        let converted_fee = calculate_fee(corrected_native_fee, fee_factor, base)?;
-
-                        // Calculate how much refund we should return.
-                        let (fee, refund) = paid.split(converted_fee);
-
-                        // Refund to the account that paid the fees. If this fails, the account might have dropped
-                        // below the existential balance. In that case we don't refund anything.
-                        let _ = <Tokens as Balanced<AccountId>>::resolve(who, refund);
-
-                        fee
-                    },
-                    Asset::CategoricalOutcome(_, _) |
-                    Asset::ScalarOutcome(_, _) |
-                    Asset::CombinatorialOutcome |
-                    Asset::PoolShare(_) => return Err(TransactionValidityError::Invalid(InvalidTransaction::Custom(8u8))),
-                };
-
+        pub struct TTCHandleCredit;
+        impl HandleCredit<AccountId, Tokens> for TTCHandleCredit {
+            fn handle_credit(final_fee: CreditOf<AccountId, Tokens>) {
                 // Handle the final fee and tip, e.g. by transferring to the treasury.
-                // Note: The `corrected_native_fee` already includes the `tip`.
                 DealWithForeignFees::on_unbalanced(final_fee);
-
-                Ok(())
             }
         }
     }
@@ -341,7 +250,7 @@ macro_rules! fee_tests {
                         class: DispatchClass::Normal,
                         pays_fee:  frame_support::dispatch::Pays::Yes,
                     };
-                    assert_eq!(TokensTxCharger::withdraw_fee(
+                    assert_eq!(<TokensTxCharger as OnChargeAssetTransaction<Runtime>>::withdraw_fee(
                         &Treasury::account_id(),
                         &mock_call,
                         &mock_dispatch_info,
@@ -397,7 +306,7 @@ macro_rules! fee_tests {
                     let corrected_native_fee = BASE;
                     let paid = <Tokens as Balanced<AccountId>>::issue(dot, 2 * BASE);
                     let tip = 0u128;
-                    assert_ok!(TokensTxCharger::correct_and_deposit_fee(
+                    assert_ok!(<TokensTxCharger as OnChargeAssetTransaction<Runtime>>::correct_and_deposit_fee(
                         &alice,
                         &mock_dispatch_info,
                         &mock_post_info,
