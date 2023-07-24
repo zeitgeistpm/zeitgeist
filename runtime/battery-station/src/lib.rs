04df9c3002af94f7538c870768475ed3dd3acd9f
Harald Heckmann <mail@haraldheckmann.de>
2023-07-24 17:48:56 +0200

    Merge release branch release-v0.3.10 into main (#1051)
    * Update versions (#1047)
    
    Update version
    
    Also removes unused code and modifies changelog layout for v0.3.10
    
    * Update weights (#1048)

--- runtime/battery-station/src/lib.rs
+++ runtime/battery-station/src/lib.rs
@@ -93,10 +93,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("zeitgeist"),
     impl_name: create_runtime_str!("zeitgeist"),
     authoring_version: 1,
-    spec_version: 46,
+    spec_version: 47,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 21,
+    transaction_version: 22,
     state_version: 1,
 };
 
