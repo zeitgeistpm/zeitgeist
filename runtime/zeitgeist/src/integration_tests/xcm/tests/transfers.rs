37d97d2210eff16543433002f3ae3b2ee3bd8d07
Harald Heckmann <mail@haraldheckmann.de>
2023-08-07 09:45:04 +0200

    Cargo fmt

--- runtime/zeitgeist/src/integration_tests/xcm/tests/transfers.rs
+++ runtime/zeitgeist/src/integration_tests/xcm/tests/transfers.rs
@@ -35,7 +35,7 @@ use orml_traits::MultiCurrency;
 use xcm::latest::{Junction, Junction::*, Junctions::*, MultiLocation, NetworkId};
 use xcm_emulator::{Limited, TestExt};
 use zeitgeist_primitives::{
-    constants::{BASE, BalanceFractionalDecimals},
+    constants::{BalanceFractionalDecimals, BASE},
     types::{CustomMetadata, XcmMetadata},
 };
 
