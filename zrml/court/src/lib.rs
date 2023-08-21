9446d344d105dc70519a027fce33e331d57940aa
Chralt <chralt.developer@gmail.com>
2023-08-21 11:33:47 +0200

    Update zrml/court/src/lib.rs
    Co-authored-by: Harald Heckmann <mail@haraldheckmann.de>

--- zrml/court/src/lib.rs
+++ zrml/court/src/lib.rs
@@ -196,8 +196,8 @@ mod pallet {
     const INITIAL_DRAWS_NUM: usize = 31;
     /// The current storage version.
     const STORAGE_VERSION: StorageVersion = StorageVersion::new(2);
-    // Weight used to increase the number of jurors for subsequent appeals
-    // of the same court.
+    /// Weight used to increase the number of jurors for subsequent appeals
+    /// of the same court.
     const APPEAL_BASIS: usize = 2;
     // Basis used to increase the bond for subsequent appeals of the same market.
     const APPEAL_BOND_BASIS: u32 = 2;
