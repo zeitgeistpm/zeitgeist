507592395c4ee419416eda83fc0fda15bcf63303
Chralt <chralt.developer@gmail.com>
2023-08-22 20:04:13 +0200

    Update zrml/court/src/lib.rs
    Co-authored-by: Harald Heckmann <mail@haraldheckmann.de>

--- zrml/court/src/lib.rs
+++ zrml/court/src/lib.rs
@@ -1308,6 +1308,19 @@ mod pallet {
                 }
             }
 
+            let remainder = total_mint.peek();
+            if total_mint.drop_zero().is_err() {
+                log::warn!(
+                    "Total issued tokens were not completely distributed, total: \
+                     {:?}, leftover: {:?}",
+                    inflation_period_mint,
+                    remainder
+                );
+                
+                T::Currency::burn(remainder);
+                debug_assert!(false);
+            }
+
             T::WeightInfo::handle_inflation(pool_len)
         }
 
