a4a7648f2e57d4a663ed1a47f4623c98078d01f3
Harald Heckmann <mail@haraldheckmann.de>
2023-08-24 21:07:19 +0200

    Use header option for benchmarks
    Also format benchmark script and moves header file into HEADER_GPL3.

--- scripts/benchmarks/quick_check.sh
+++ scripts/benchmarks/quick_check.sh
@@ -23,7 +23,7 @@ export ZEITGEIST_PALLETS_RUNS=0
 
 export PROFILE=release
 export PROFILE_DIR=release
-export ADDITIONAL=--detailed-log-output
+export ADDITIONAL_PARAMS=--detailed-log-output
 export EXECUTION=native
 # TODO(#848) Delete this, when global disputes is on main-net
 export ADDITIONAL_FEATURES=with-global-disputes
