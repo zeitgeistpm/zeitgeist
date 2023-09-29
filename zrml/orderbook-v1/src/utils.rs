364798098c36865166a17729fe838f53912d565f
Chralt98 <chralt98@gmail.com>
2023-09-29 06:02:17 +0200

    fix clippy

--- zrml/orderbook-v1/src/utils.rs
+++ zrml/orderbook-v1/src/utils.rs
@@ -21,32 +21,36 @@ use crate::*;
 use frame_support::traits::Currency;
 use sp_runtime::traits::AccountIdConversion;
 use zeitgeist_primitives::traits::MarketCommonsPalletApi;
+use zeitgeist_primitives::types::{
+    Asset, Market, MarketCreation, MarketDisputeMechanism, MarketPeriod, MarketStatus,
+    MarketType, ScoringRule, Deadlines,
+};
 
 type CurrencyOf<T> = <<T as Config>::MarketCommons as MarketCommonsPalletApi>::Currency;
 type BalanceOf<T> = <CurrencyOf<T> as Currency<<T as frame_system::Config>::AccountId>>::Balance;
-type MarketOf<T> = zeitgeist_primitives::types::Market<
+type MarketOf<T> = Market<
     <T as frame_system::Config>::AccountId,
     BalanceOf<T>,
     <T as frame_system::Config>::BlockNumber,
     MomentOf<T>,
-    zeitgeist_primitives::types::Asset<MarketIdOf<T>>,
+    Asset<MarketIdOf<T>>,
 >;
 
 pub(crate) fn market_mock<T>() -> MarketOf<T>
 where
     T: crate::Config,
 {
-    zeitgeist_primitives::types::Market {
-        base_asset: zeitgeist_primitives::types::Asset::Ztg,
-        creation: zeitgeist_primitives::types::MarketCreation::Permissionless,
+    Market {
+        base_asset: Asset::Ztg,
+        creation: MarketCreation::Permissionless,
         creator_fee: sp_runtime::Perbill::zero(),
         creator: T::PalletId::get().into_account_truncating(),
-        market_type: zeitgeist_primitives::types::MarketType::Categorical(64u16),
-        dispute_mechanism: zeitgeist_primitives::types::MarketDisputeMechanism::Authorized,
+        market_type: MarketType::Categorical(64u16),
+        dispute_mechanism: Some(MarketDisputeMechanism::Authorized),
         metadata: Default::default(),
         oracle: T::PalletId::get().into_account_truncating(),
-        period: zeitgeist_primitives::types::MarketPeriod::Block(Default::default()),
-        deadlines: zeitgeist_primitives::types::Deadlines {
+        period: MarketPeriod::Block(Default::default()),
+        deadlines: Deadlines {
             grace_period: 1_u32.into(),
             oracle_duration: 1_u32.into(),
             dispute_duration: 1_u32.into(),
@@ -54,7 +58,7 @@ where
         report: None,
         resolved_outcome: None,
         scoring_rule: ScoringRule::Orderbook,
-        status: zeitgeist_primitives::types::MarketStatus::Active,
+        status: MarketStatus::Active,
         bonds: Default::default(),
     }
 }
