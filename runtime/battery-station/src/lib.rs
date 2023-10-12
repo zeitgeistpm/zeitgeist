37a91e006c84c433647f42ccb905ade3ce58d04d
Harald Heckmann <mail@haraldheckmann.de>
2023-10-12 18:57:10 +0200

    Update version (#1142)
    * Update version
    
    * Bump all RuntimeVersion
    
    * Revert "Bump all RuntimeVersion"
    
    This reverts commit 80761b855b3bb6dbbc1767adf8713b4e62014b3b.
    
    * Bump BS RuntimeVersion

--- runtime/battery-station/src/lib.rs
+++ runtime/battery-station/src/lib.rs
@@ -105,10 +105,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("zeitgeist"),
     impl_name: create_runtime_str!("zeitgeist"),
     authoring_version: 1,
-    spec_version: 49,
+    spec_version: 50,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 24,
+    transaction_version: 25,
     state_version: 1,
 };
 
