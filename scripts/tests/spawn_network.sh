ecfceb881e9308d074f42b693d4310941c67c0e2
Chralt98 <chralt98@gmail.com>
2023-10-19 16:10:21 +0200

    outsource function

--- scripts/tests/spawn_network.sh
+++ scripts/tests/spawn_network.sh
@@ -18,49 +18,55 @@ if [[ -f "${ZOMBIENET_BINARY}" ]]; then
     exit 0
 fi
 
-# Get the appropriate download link based on the OS
-case "$(uname -s)" in
-    Linux)
-        ARCHITECTURE="$(uname -m)"
-        if [[ "${ARCHITECTURE}" == "x86_64" ]]; then
-            FILE_NAME="zombienet-linux-x64"
-        elif [[ "${ARCHITECTURE}" == "aarch64" ]]; then
-            FILE_NAME="zombienet-linux-arm64"
-        else
-            echo "Unsupported architecture."
+function download_zombienet {
+    # Get the appropriate download link based on the OS
+    case "$(uname -s)" in
+        Linux)
+            ARCHITECTURE="$(uname -m)"
+            if [[ "${ARCHITECTURE}" == "x86_64" ]]; then
+                FILE_NAME="zombienet-linux-x64"
+            elif [[ "${ARCHITECTURE}" == "aarch64" ]]; then
+                FILE_NAME="zombienet-linux-arm64"
+            else
+                echo "Unsupported architecture."
+                exit 1
+            fi
+            ;;
+        Darwin)
+            FILE_NAME="zombienet-macos"
+            ;;
+        *)
+            echo "Unsupported operating system."
             exit 1
-        fi
-        ;;
-    Darwin)
-        FILE_NAME="zombienet-macos"
-        ;;
-    *)
-        echo "Unsupported operating system."
+            ;;
+    esac
+
+    # Fetch the latest release download URL from GitHub
+    DOWNLOAD_URL=$(curl -s https://api.github.com/repos/paritytech/zombienet/releases/latest | \
+                jq -r ".assets[] | select(.name == \"${FILE_NAME}\") | .browser_download_url")
+
+    if [[ -z "${DOWNLOAD_URL}" ]]; then
+        echo "Failed to retrieve download URL."
         exit 1
-        ;;
-esac
+    fi
 
-# Fetch the latest release download URL from GitHub
-DOWNLOAD_URL=$(curl -s https://api.github.com/repos/paritytech/zombienet/releases/latest | \
-               jq -r ".assets[] | select(.name == \"${FILE_NAME}\") | .browser_download_url")
+    mkdir -p ./tmp/
 
-if [[ -z "${DOWNLOAD_URL}" ]]; then
-    echo "Failed to retrieve download URL."
-    exit 1
-fi
+    # Download the file
+    echo "Downloading ${FILE_NAME} from ${DOWNLOAD_URL}"
+    curl -L "${DOWNLOAD_URL}" -o "${ZOMBIENET_BINARY}"
 
-mkdir -p ./tmp/
+    # Provide feedback on the download status
+    if [[ $? -eq 0 ]]; then
+        echo "Download successful!"
+    else
+        echo "Download failed!"
+        exit 1
+    fi
+}
 
-# Download the file
-echo "Downloading ${FILE_NAME} from ${DOWNLOAD_URL}"
-curl -L "${DOWNLOAD_URL}" -o "${ZOMBIENET_BINARY}"
-
-# Provide feedback on the download status
-if [[ $? -eq 0 ]]; then
-    echo "Download successful!"
-else
-    echo "Download failed!"
-    exit 1
+if [ ! -f ${ZOMBIENET_BINARY} ]; then
+    download_zombienet $ZOMBIENET_BINARY
 fi
 
 # Make the file executable
