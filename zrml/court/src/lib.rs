fc00ac748dfa03a43926d6fc630cc1547efadc04
Chralt98 <chralt98@gmail.com>
2023-10-23 11:53:37 +0200

    add log targets to runtime pallets

--- zrml/court/src/lib.rs
+++ zrml/court/src/lib.rs
@@ -196,6 +196,7 @@ mod pallet {
     const INITIAL_DRAWS_NUM: usize = 31;
     /// The current storage version.
     const STORAGE_VERSION: StorageVersion = StorageVersion::new(2);
+    const LOG_TARGET: &str = "runtime::zrml-court";
     /// Weight used to increase the number of jurors for subsequent appeals
     /// of the same court.
     const APPEAL_BASIS: usize = 2;
@@ -1075,6 +1076,7 @@ mod pallet {
                     <Participants<T>>::insert(&draw.court_participant, p_info);
                 } else {
                     log::warn!(
+                        target: LOG_TARGET,
                         "Participant {:?} not found in Participants storage \
                          (reassign_court_stakes). Court id {:?}.",
                         draw.court_participant,
@@ -1271,6 +1273,7 @@ mod pallet {
                 .saturated_into::<BalanceOf<T>>();
             if inflation_period_mint >= log_threshold {
                 log::warn!(
+                    target: LOG_TARGET,
                     "Inflation per period is greater than the threshold. Inflation per period: \
                      {:?}, threshold: {:?}",
                     inflation_period_mint,
@@ -1323,6 +1326,7 @@ mod pallet {
             let remainder = total_mint.peek();
             if total_mint.drop_zero().is_err() {
                 log::debug!(
+                    target: LOG_TARGET,
                     "Total issued tokens were not completely distributed, total: {:?}, leftover: \
                      {:?}",
                     inflation_period_mint,
@@ -1695,6 +1699,7 @@ mod pallet {
                     <Participants<T>>::insert(&old_draw.court_participant, p_info);
                 } else {
                     log::warn!(
+                        target: LOG_TARGET,
                         "Participant {:?} not found in Participants storage \
                          (unlock_participants_from_last_draw). Court id {:?}.",
                         old_draw.court_participant,
