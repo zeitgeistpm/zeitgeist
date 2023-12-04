050c36bd9048a8d08977d56616d79dc28946437b
Malte Kliemann <mail@maltekliemann.com>
2023-12-04 14:15:06 +0100

    Format code

--- zrml/neo-swaps/src/mock.rs
+++ zrml/neo-swaps/src/mock.rs
@@ -484,33 +484,31 @@ impl ExtBuilder {
         pallet_balances::GenesisConfig::<Runtime> { balances: self.balances }
             .assimilate_storage(&mut t)
             .unwrap();
-        #[cfg(feature = "parachain")] 
+        #[cfg(feature = "parachain")]
         {
-                use frame_support::traits::GenesisBuild;
-                orml_tokens::GenesisConfig::<Runtime> {
-                    balances: vec![(ALICE, FOREIGN_ASSET, _101)],
-                }
+            use frame_support::traits::GenesisBuild;
+            orml_tokens::GenesisConfig::<Runtime> { balances: vec![(ALICE, FOREIGN_ASSET, _101)] }
                 .assimilate_storage(&mut t)
                 .unwrap();
-                let custom_metadata = zeitgeist_primitives::types::CustomMetadata {
-                    allow_as_base_asset: true,
-                    ..Default::default()
-                };
-                orml_asset_registry_mock::GenesisConfig {
-                    metadata: vec![(
-                        FOREIGN_ASSET,
-                        AssetMetadata {
-                            decimals: 18,
-                            name: "MKL".as_bytes().to_vec(),
-                            symbol: "MKL".as_bytes().to_vec(),
-                            existential_deposit: 0,
-                            location: None,
-                            additional: custom_metadata,
-                        },
-                    )],
-                }
-                .assimilate_storage(&mut t)
-                .unwrap();
+            let custom_metadata = zeitgeist_primitives::types::CustomMetadata {
+                allow_as_base_asset: true,
+                ..Default::default()
+            };
+            orml_asset_registry_mock::GenesisConfig {
+                metadata: vec![(
+                    FOREIGN_ASSET,
+                    AssetMetadata {
+                        decimals: 18,
+                        name: "MKL".as_bytes().to_vec(),
+                        symbol: "MKL".as_bytes().to_vec(),
+                        existential_deposit: 0,
+                        location: None,
+                        additional: custom_metadata,
+                    },
+                )],
+            }
+            .assimilate_storage(&mut t)
+            .unwrap();
         }
         let mut test_ext: sp_io::TestExternalities = t.into();
         test_ext.execute_with(|| System::set_block_number(1));
