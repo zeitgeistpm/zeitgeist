c2ebd4fb502c9f93d935733f2f78c495af85057a
Malte Kliemann <mail@maltekliemann.com>
2023-12-12 23:13:10 +0100

    Use hotfixed `exp`

--- zrml/neo-swaps/src/math.rs
+++ zrml/neo-swaps/src/math.rs
@@ -15,11 +15,13 @@
 // You should have received a copy of the GNU General Public License
 // along with Zeitgeist. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::{BalanceOf, Config, Error};
+use crate::{
+    math::transcendental::{exp, ln},
+    BalanceOf, Config, Error,
+};
 use alloc::vec::Vec;
 use core::marker::PhantomData;
 use fixed::FixedU128;
-use hydra_dx_math::transcendental::{exp, ln};
 use sp_runtime::{DispatchError, SaturatedConversion};
 use typenum::U80;
 
@@ -99,6 +101,88 @@ impl<T: Config> MathOps<T> for Math<T> {
         let liquidity = liquidity.saturated_into();
         let spot_prices = spot_prices.into_iter().map(|p| p.saturated_into()).collect();
         Ok((liquidity, spot_prices))
+    }
+}
+
+mod transcendental {
+    use fixed::traits::FixedUnsigned;
+    pub(crate) use hydra_dx_math::transcendental::{exp as inner_exp, ln};
+    use sp_runtime::traits::One;
+
+    pub(crate) fn exp<S, D>(operand: S, neg: bool) -> Result<D, ()>
+    where
+        S: FixedUnsigned + PartialOrd<D> + One,
+        D: FixedUnsigned + PartialOrd<S> + From<S> + One,
+    {
+        if operand == S::one() && neg {
+            let e_inverse =
+                S::from_str("0.367879441171442321595523770161460867445").map_err(|_| ())?;
+            return Ok(D::from(e_inverse));
+        }
+        inner_exp(operand, neg)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use alloc::str::FromStr;
+        use fixed::types::U64F64;
+        use test_case::test_case;
+
+        type S = U64F64;
+        type D = U64F64;
+
+        #[test_case("0", false, "1")]
+        #[test_case("0", true, "1")]
+        #[test_case("1", false, "2.718281828459045235360287471352662497757")]
+        #[test_case("1", true, "0.367879441171442321595523770161460867445")]
+        #[test_case("2", false, "7.3890560989306502265")]
+        #[test_case("2", true, "0.13533528323661269186")]
+        #[test_case("0.1", false, "1.1051709180756476246")]
+        #[test_case("0.1", true, "0.9048374180359595733")]
+        #[test_case("0.9", false, "2.4596031111569496633")]
+        #[test_case("0.9", true, "0.40656965974059911195")]
+        #[test_case("1.5", false, "4.481689070338064822")]
+        #[test_case("1.5", true, "0.22313016014842982894")]
+        #[test_case("3.3", false, "27.1126389206578874259")]
+        #[test_case("3.3", true, "0.03688316740124000543")]
+        #[test_case("7.3456", false, "1549.3643050275008503592")]
+        #[test_case("7.3456", true, "0.00064542599616831253")]
+        #[test_case("12.3456789", false, "229964.194569082134542849")]
+        #[test_case("12.3456789", true, "0.00000434850304358833")]
+        #[test_case("13", false, "442413.39200892050332603603")]
+        #[test_case("13", true, "0.0000022603294069810542")]
+        fn exp_works(operand: &str, neg: bool, expected: &str) {
+            let o = U64F64::from_str(operand).unwrap();
+            let e = U64F64::from_str(expected).unwrap();
+            assert_eq!(exp::<S, D>(o, neg).unwrap(), e);
+        }
+
+        #[test_case("1", "0", false)]
+        #[test_case("2", "0.69314718055994530943", false)]
+        #[test_case("3", "1.09861228866810969136", false)]
+        #[test_case("2.718281828459045235360287471352662497757", "1", false)]
+        #[test_case("1.1051709180756476246", "0.09999999999999999975", false)]
+        #[test_case("2.4596031111569496633", "0.89999999999999999976", false)]
+        #[test_case("4.481689070338064822", "1.49999999999999999984", false)]
+        #[test_case("27.1126389206578874261", "3.3", false)]
+        #[test_case("1549.3643050275008503592", "7.34560000000000000003", false)]
+        #[test_case("229964.194569082134542849", "12.3456789000000000002", false)]
+        #[test_case("442413.39200892050332603603", "13.0000000000000000002", false)]
+        #[test_case("0.9048374180359595733", "0.09999999999999999975", true)]
+        #[test_case("0.40656965974059911195", "0.8999999999999999998", true)]
+        #[test_case("0.22313016014842982894", "1.4999999999999999999", true)]
+        #[test_case("0.03688316740124000543", "3.3000000000000000005", true)]
+        #[test_case("0.00064542599616831253", "7.34560000000000002453", true)]
+        #[test_case("0.00000434850304358833", "12.34567890000000711117", true)]
+        #[test_case("0.0000022603294069810542", "13.0000000000000045352", true)]
+        fn ln_works(operand: &str, expected_abs: &str, expected_neg: bool) {
+            let o = U64F64::from_str(operand).unwrap();
+            let e = U64F64::from_str(expected_abs).unwrap();
+            let (a, n) = ln::<S, D>(o).unwrap();
+            assert_eq!(a, e);
+            assert_eq!(n, expected_neg);
+        }
     }
 }
 
