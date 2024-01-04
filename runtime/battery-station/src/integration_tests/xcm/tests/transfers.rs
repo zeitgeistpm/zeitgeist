27e12e60e3ce68ba38b16dcbe59c268bd90265fe
Harald Heckmann <mail@haraldheckmann.de>
2024-01-04 21:16:36 +0100

    Update copyright

--- runtime/battery-station/src/integration_tests/xcm/tests/transfers.rs
+++ runtime/battery-station/src/integration_tests/xcm/tests/transfers.rs
@@ -1,4 +1,4 @@
-// Copyright 2022-2023 Forecasting Technologies LTD.
+// Copyright 2022-2024 Forecasting Technologies LTD.
 // Copyright 2021 Centrifuge Foundation (centrifuge.io).
 //
 // This file is part of Zeitgeist.
