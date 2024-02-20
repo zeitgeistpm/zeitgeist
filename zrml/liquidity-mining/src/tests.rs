f21512857bb396487a298f475e00d671b47b82ed
Harald Heckmann <mail@haraldheckmann.de>
2024-02-20 19:47:45 +0100

    Update liquidity-mining

--- zrml/liquidity-mining/src/tests.rs
+++ zrml/liquidity-mining/src/tests.rs
@@ -32,8 +32,8 @@ use frame_support::{
 };
 use frame_system::RawOrigin;
 use zeitgeist_primitives::types::{
-    Asset, Deadlines, Market, MarketBonds, MarketCreation, MarketDisputeMechanism, MarketPeriod,
-    MarketStatus, MarketType, ScoringRule,
+    BaseAsset, Deadlines, Market, MarketBonds, MarketCreation, MarketDisputeMechanism,
+    MarketPeriod, MarketStatus, MarketType, ScoringRule,
 };
 use zrml_market_commons::Markets;
 
@@ -203,7 +203,7 @@ fn create_default_market(market_id: u128, period: Range<u64>) {
     Markets::<Runtime>::insert(
         market_id,
         Market {
-            base_asset: Asset::Ztg,
+            base_asset: BaseAsset::Ztg,
             creation: MarketCreation::Permissionless,
             creator_fee: sp_runtime::Perbill::zero(),
             creator: 0,
