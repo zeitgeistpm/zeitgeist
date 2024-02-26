f70af620aca4bc5fb79bfd5deb7444453bbbdff0
Malte Kliemann <mail@maltekliemann.com>
2024-02-27 00:59:06 +0100

    Update copyright notices

--- zrml/authorized/src/mock_storage.rs
+++ zrml/authorized/src/mock_storage.rs
@@ -1,4 +1,4 @@
-// Copyright 2023 Forecasting Technologies LTD.
+// Copyright 2023-2024 Forecasting Technologies LTD.
 // Copyright 2022 Forecasting Technologies LTD.
 //
 // This file is part of Zeitgeist.
