724d77590dbd6a0c9d361c8fc2009fb77633ae8a
Harald Heckmann <mail@haraldheckmann.de>
2024-04-15 12:38:58 +0200

    Update runtime version

--- runtime/battery-station/src/lib.rs
+++ runtime/battery-station/src/lib.rs
@@ -105,10 +105,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("zeitgeist"),
     impl_name: create_runtime_str!("zeitgeist"),
     authoring_version: 1,
-    spec_version: 54,
+    spec_version: 55,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 28,
+    transaction_version: 29,
     state_version: 1,
 };
 
