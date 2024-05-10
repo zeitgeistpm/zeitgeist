c855201d90d12d11fcc8f6aecfd537a51c9cd042
Chralt98 <chralt98@gmail.com>
2024-05-10 15:42:01 +0200

    simplify xcm pattern matching

--- runtime/zeitgeist/src/xcm_config/hydra_atomic_swap.rs
+++ runtime/zeitgeist/src/xcm_config/hydra_atomic_swap.rs
@@ -17,104 +17,65 @@
 #![cfg(feature = "parachain")]
 
 use crate::RuntimeCall;
-use frame_support::traits::Contains;
+use frame_support::{parameter_types, traits::Contains};
 use xcm::prelude::*;
 
-pub const HYDRA_DX_PARA_ID: u32 = 2034;
+parameter_types! {
+    pub const HydraDxParachainId: u32 = 2034;
+    pub HydraDxMultiLocation: MultiLocation = MultiLocation::new(1, X1(Parachain(HydraDxParachainId::get())));
+}
 
 pub struct AllowHydraDxAtomicSwap;
 
 impl Contains<(MultiLocation, Xcm<RuntimeCall>)> for AllowHydraDxAtomicSwap {
     fn contains((ref origin, ref msg): &(MultiLocation, Xcm<RuntimeCall>)) -> bool {
-        // TODO outsource this into parameter_types!
-        let hydra_dx_loc: MultiLocation = MultiLocation::new(1, X1(Parachain(HYDRA_DX_PARA_ID)));
-        let matches_inner_0 = |xcm: Xcm<()>| -> bool {
-            let mut xit = xcm.inner().iter();
-            match (xit.next(), xit.next(), xit.next()) {
-                (Some(BuyExecution { .. }), Some(DepositAsset { .. }), None) => true,
-                _ => false,
-            }
-        };
-        let matches_inner_1 = |xcm: Xcm<()>| -> Option<Xcm<()>> {
-            let mut xit = xcm.inner().iter();
-            match (xit.next(), xit.next(), xit.next()) {
-                (
-                    Some(BuyExecution { .. }),
-                    // TODO maybe dest verification?
-                    Some(DepositReserveAsset { assets: _, dest: _, xcm }),
-                    None,
-                ) => Some(xcm.clone()),
-                _ => None,
-            }
-        };
-        let matches_inner_2 = |xcm: Xcm<()>| -> Option<Xcm<()>> {
-            let mut xit = xcm.inner().iter();
-            match (xit.next(), xit.next(), xit.next(), xit.next()) {
-                (
-                    Some(BuyExecution { .. }),
-                    Some(ExchangeAsset { .. }),
-                    Some(InitiateReserveWithdraw { assets: _, reserve: _, xcm }),
-                    None,
-                ) => Some(xcm.clone()),
-                _ => None,
-            }
-        };
-        let matches_inner_3 = |xcm: &Xcm<()>| -> Option<Xcm<()>> {
-            let mut xit = xcm.inner().iter();
-            match (xit.next(), xit.next(), xit.next()) {
-                (
-                    Some(BuyExecution { .. }),
-                    Some(DepositReserveAsset { assets: _, dest, xcm }),
-                    None,
-                ) if *dest == hydra_dx_loc => Some(xcm.clone()),
-                _ => None,
-            }
-        };
         match origin {
             MultiLocation { parents: 0, interior: Junctions::X1(AccountId32 { .. }) } => {
                 // TODO figure out which stablecoin pairs to swap atomically with HydraDX
-                let mut it = msg.inner().iter();
-                match (it.next(), it.next(), it.next()) {
-                    (
-                        Some(SetFeesMode { jit_withdraw: true }),
-                        Some(TransferReserveAsset { assets: _, dest, xcm }),
-                        None,
-                    ) => {
-                        let mut xit = xcm.inner().iter();
-                        let valid_xcm = match (xit.next(), xit.next(), xit.next(), xit.next()) {
-                            (
-                                Some(BuyExecution { .. }),
-                                Some(ExchangeAsset { .. }),
-                                Some(DepositAsset { .. }),
-                                None,
-                            ) => true,
+                match msg.inner() {
+                    [
+                        SetFeesMode { jit_withdraw: true },
+                        TransferReserveAsset { assets: _, dest, xcm },
+                    ] => {
+                        let valid_xcm = match xcm.inner() {
+                            [BuyExecution { .. }, ExchangeAsset { .. }, DepositAsset { .. }] => {
+                                true
+                            }
                             _ => false,
                         };
-                        let valid_dest = *dest == hydra_dx_loc;
+                        let valid_dest = *dest == HydraDxMultiLocation::get();
                         // TODO Do we want to check the assets from TransferReserveAsset? If yes, we could check this here too!
                         return valid_xcm && valid_dest;
                     }
-                    (
-                        Some(WithdrawAsset(_)),
-                        Some(InitiateReserveWithdraw { assets: _, reserve: _, xcm }),
-                        None,
-                    ) => {
-                        let xcm_3_opt = matches_inner_3(xcm);
-                        let xcm_3 = match xcm_3_opt {
-                            None => return false,
-                            Some(xcm_3) => xcm_3,
+                    [WithdrawAsset(_), InitiateReserveWithdraw { assets: _, reserve: _, xcm }] => {
+                        let xcm_3 = match xcm.inner() {
+                            [BuyExecution { .. }, DepositReserveAsset { assets: _, dest, xcm }]
+                                if *dest == HydraDxMultiLocation::get() =>
+                            {
+                                xcm
+                            }
+                            _ => return false,
                         };
-                        let xcm_2_opt = matches_inner_2(xcm_3);
-                        let xcm_2 = match xcm_2_opt {
-                            None => return false,
-                            Some(xcm_2) => xcm_2,
+                        let xcm_2 = match xcm_3.inner() {
+                            [
+                                BuyExecution { .. },
+                                ExchangeAsset { .. },
+                                InitiateReserveWithdraw { assets: _, reserve: _, xcm },
+                            ] => xcm,
+                            _ => return false,
                         };
-                        let xcm_1_opt = matches_inner_1(xcm_2);
-                        let xcm_1 = match xcm_1_opt {
-                            None => return false,
-                            Some(xcm_1) => xcm_1,
+                        let xcm_1 = match xcm_2.inner() {
+                            // TODO maybe dest verification?
+                            [
+                                BuyExecution { .. },
+                                DepositReserveAsset { assets: _, dest: _, xcm },
+                            ] => xcm,
+                            _ => return false,
                         };
-                        return matches_inner_0(xcm_1);
+                        return match xcm_1.inner() {
+                            [BuyExecution { .. }, DepositAsset { .. }] => true,
+                            _ => false,
+                        };
                     }
                     _ => return false,
                 }
