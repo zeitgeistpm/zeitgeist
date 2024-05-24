0789a8f7b5d844fcb1a8d4c49d2d489a4e078983
Malte Kliemann <mail@maltekliemann.com>
2024-05-24 22:46:03 +0200

    Fix licenses and comments

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -35,6 +35,25 @@
 //
 //     You should have received a copy of the GNU General Public License
 //     along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// This file incorporates work covered by the following copyright and
+// permission notice:
+//
+//     Copyright (C) Parity Technologies (UK) Ltd.
+//     SPDX-License-Identifier: Apache-2.0
+//
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//
+//     	http://www.apache.org/licenses/LICENSE-2.0
+//
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![recursion_limit = "512"]
@@ -107,8 +126,8 @@ macro_rules! decl_common_types {
 
         type TrieId = BoundedVec<u8, ConstU32<128>>;
 
-        /// Information for managing an account and its sub trie abstraction.
-        /// This is the required info to cache for an account.
+        // `ContractInfo` struct that we need for `ClearContractsChildTries` but pallet-contracts
+        // doesn't expose publicly.
         #[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
         pub struct ContractInfo {
             pub trie_id: TrieId,
