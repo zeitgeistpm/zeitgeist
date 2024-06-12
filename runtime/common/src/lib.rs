e19fcee8d756348d9f3f9b0c24cd57365f995ae1
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2024-06-12 12:01:37 +0530

    Add hybrid-router calls to HandleAssets

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -992,7 +992,9 @@ macro_rules! impl_config_traits {
                     ),
                     ProxyType::HandleAssets => matches!(
                         c,
-                        RuntimeCall::NeoSwaps(zrml_neo_swaps::Call::join { .. })
+                        RuntimeCall::HybridRouter(zrml_hybrid_router::Call::buy { .. })
+                            | RuntimeCall::HybridRouter(zrml_hybrid_router::Call::sell { .. })
+                            | RuntimeCall::NeoSwaps(zrml_neo_swaps::Call::join { .. })
                             | RuntimeCall::NeoSwaps(zrml_neo_swaps::Call::exit { .. })
                             | RuntimeCall::NeoSwaps(zrml_neo_swaps::Call::buy { .. })
                             | RuntimeCall::NeoSwaps(zrml_neo_swaps::Call::sell { .. })
