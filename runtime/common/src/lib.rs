2bd5e6c2adc88e0b89ba66a624c15857f75d7921
Malte Kliemann <mail@maltekliemann.com>
2024-09-24 16:10:40 +0200

    Scaffold combo pallet (#1367)
    * Scaffold combo pallet
    
    * Fix dependencies/features

--- runtime/common/src/lib.rs
+++ runtime/common/src/lib.rs
@@ -358,6 +358,7 @@ macro_rules! create_runtime {
                 Orderbook: zrml_orderbook::{Call, Event<T>, Pallet, Storage} = 61,
                 Parimutuel: zrml_parimutuel::{Call, Event<T>, Pallet, Storage} = 62,
                 HybridRouter: zrml_hybrid_router::{Call, Event<T>, Pallet, Storage} = 64,
+                Combo: zrml_combo::{Pallet, Storage} = 65,
 
                 $($additional_pallets)*
             }
@@ -1167,6 +1168,8 @@ macro_rules! impl_config_traits {
             type PalletId = AuthorizedPalletId;
             type WeightInfo = zrml_authorized::weights::WeightInfo<Runtime>;
         }
+
+        impl zrml_combo::Config for Runtime {}
 
         impl zrml_court::Config for Runtime {
             type AppealBond = AppealBond;
