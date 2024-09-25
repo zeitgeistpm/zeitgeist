09ff3a802a91b0eebdd82c756e5887f82b36d618
Malte Kliemann <mail@maltekliemann.com>
2024-09-25 16:08:50 +0200

    Rough outline of `CryptographicIdManager` implementation

--- zrml/combo/src/types/cryptographic_id_manager.rs
+++ zrml/combo/src/types/cryptographic_id_manager.rs
@@ -1,25 +1,26 @@
 use crate::traits::IdManager;
 use core::marker::PhantomData;
 use ethnum::U256;
-use frame_support::Blake2_256;
+use frame_support::{Blake2_256, StorageHasher};
 use sp_runtime::DispatchError;
-use zeitgeist_primitives::Asset;
+use zeitgeist_primitives::types::Asset;
 
 pub(crate) struct CryptographicIdManager<MarketId>(PhantomData<MarketId>);
 
 impl<MarketId> IdManager for CryptographicIdManager<MarketId>
 where
-    MarketId: Into<U256>,
+    MarketId: Into<U256> + MaybeToBytes,
 {
     type Asset = Asset<MarketId>;
     type Id = U256;
     type MarketId = MarketId;
 
     fn get_collection_id(
-        parent_collection_id: Option<Self::Id>,
+        _parent_collection_id: Option<Self::Id>,
         market_id: Self::MarketId,
         index_set: Vec<bool>,
     ) -> Result<Self::Id, DispatchError> {
+        let _hash = hash_pair((market_id, index_set));
         Ok(0u8.into())
     }
 
@@ -27,23 +28,30 @@ where
         collateral: Self::Asset,
         collection_id: Self::Id,
     ) -> Result<Self::Id, DispatchError> {
-        let bytes = hash_pair((collateral, collection_id))?;
+        let bytes_vec =
+            hash_pair((collateral, collection_id)).ok_or(DispatchError::Other("TODO"))?;
 
-        U256::from_be_slice(&bytes)
+        let bytes_slice =
+            <[u8; 32]>::try_from(&bytes_vec[..32]).map_err(|_| DispatchError::Other("TODO"))?;
+
+        Ok(U256::from_be_bytes(bytes_slice))
     }
 }
 
+// TODO Replace pair with parameters.
 fn hash_pair<T1, T2>(pair: (T1, T2)) -> Option<Vec<u8>>
 where
     T1: MaybeToBytes,
     T2: MaybeToBytes,
 {
-    let result = Vec::new();
+    let mut bytes = Vec::new();
 
-    result.extend_from_slice(pair.0.maybe_to_bytes()?);
-    result.extend_from_slice(pair.1.maybe_to_bytes()?);
+    bytes.extend_from_slice(&pair.0.maybe_to_bytes()?);
+    bytes.extend_from_slice(&pair.1.maybe_to_bytes()?);
 
-    result
+    let result = Blake2_256::hash(&bytes).to_vec();
+
+    Some(result)
 }
 
 // TODO Move into traits!
@@ -52,14 +60,49 @@ trait MaybeToBytes {
     fn maybe_to_bytes(&self) -> Option<Vec<u8>>;
 }
 
+// TODO Use macros for this
+impl MaybeToBytes for u32 {
+    fn maybe_to_bytes(&self) -> Option<Vec<u8>> {
+        Some(self.to_be_bytes().to_vec())
+    }
+}
+
 impl MaybeToBytes for u128 {
     fn maybe_to_bytes(&self) -> Option<Vec<u8>> {
-        Some(self.to_be_bytes())
+        Some(self.to_be_bytes().to_vec())
     }
 }
 
 impl MaybeToBytes for U256 {
-    fn maybe_to_bytes(&self) -> Option<Vec<u8>> {}
+    fn maybe_to_bytes(&self) -> Option<Vec<u8>> {
+        Some(self.to_be_bytes().to_vec())
+    }
+}
+
+impl MaybeToBytes for bool {
+    fn maybe_to_bytes(&self) -> Option<Vec<u8>> {
+        let result = match self {
+            true => vec![1],
+            false => vec![0],
+        };
+
+        Some(result)
+    }
+}
+
+impl<T> MaybeToBytes for Vec<T>
+where
+    T: MaybeToBytes,
+{
+    fn maybe_to_bytes(&self) -> Option<Vec<u8>> {
+        let mut result = Vec::new();
+
+        for b in self.iter() {
+            result.extend_from_slice(&b.maybe_to_bytes()?);
+        }
+
+        Some(result)
+    }
 }
 
 /// Beware! All changes to this function need to be backwards compatible. Failure to follow this
@@ -67,8 +110,8 @@ impl MaybeToBytes for U256 {
 impl<MarketId> MaybeToBytes for Asset<MarketId> {
     fn maybe_to_bytes(&self) -> Option<Vec<u8>> {
         let pair = match self {
-            Ztg => (0, 0),
-            ForeignAsset(id) => (1, id),
+            Asset::Ztg => (0u32, 0u8.into()),
+            Asset::ForeignAsset(id) => (1u32, *id),
             _ => return None,
         };
 
