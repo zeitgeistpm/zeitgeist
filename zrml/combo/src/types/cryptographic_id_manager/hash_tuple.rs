ec09aa19d227f4c32e4118e54a1f7480e7e68472
Malte Kliemann <mail@maltekliemann.com>
2024-09-29 12:22:41 +0200

    Clean up and tests

--- zrml/combo/src/types/cryptographic_id_manager/hash_tuple.rs
+++ zrml/combo/src/types/cryptographic_id_manager/hash_tuple.rs
@@ -16,9 +16,7 @@ where
     Blake2_256::hash(&bytes)
 }
 
-// TODO Move into traits!
 pub trait ToBytes {
-    /// `None` indicates failure.
     fn to_bytes(&self) -> Vec<u8>;
 }
 
@@ -73,5 +71,33 @@ where
 {
     fn to_bytes(&self) -> Vec<u8> {
         self.encode()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    type MarketId = u128;
+
+    // Beware! If you have to modify these tests, that means that you broke encoding of assets in a
+    // way that's not backwards compatible.
+    #[test_case(Asset::Ztg, vec![4])]
+    #[test_case(Asset::ForeignAsset(0), vec![5, 0, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(1), vec![5, 1, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(2), vec![5, 2, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(3), vec![5, 3, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(4), vec![5, 4, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(5), vec![5, 5, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(6), vec![5, 6, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(7), vec![5, 7, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(8), vec![5, 8, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(9), vec![5, 9, 0, 0, 0])]
+    #[test_case(Asset::ForeignAsset(u32::MAX - 1), vec![5, 254, 255, 255, 255])]
+    #[test_case(Asset::ForeignAsset(u32::MAX), vec![5, 255, 255, 255, 255])]
+    fn asset_to_bytes_works(asset: Asset<MarketId>, expected: Vec<u8>) {
+        let actual = asset.to_bytes();
+        assert_eq!(actual, expected);
     }
 }
