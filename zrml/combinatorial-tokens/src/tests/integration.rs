dd17bfa42a84cb1b8d48dbc53aef5245f75babba
Malte Kliemann <mail@maltekliemann.com>
2024-10-09 17:47:38 +0200

    Add more integration tests

--- zrml/combinatorial-tokens/src/tests/integration.rs
+++ zrml/combinatorial-tokens/src/tests/integration.rs
@@ -4,10 +4,20 @@ use super::*;
 fn split_followed_by_merge_vertical_no_parent() {
     ExtBuilder::build().execute_with(|| {
         let alice = Account::new(0).deposit(Asset::Ztg, _100).unwrap();
+        let pallet = Account::new(Pallet::<Runtime>::account_id());
 
         let market_id = create_market(Asset::Ztg, MarketType::Categorical(3));
-        let partition = vec![vec![B1, B0, B1], vec![B0, B1, B0]];
+        let partition = vec![vec![B0, B0, B1], vec![B1, B1, B0]];
         let amount = _1;
+
+        let ct_001 = CombinatorialToken([
+            207, 168, 160, 93, 238, 221, 197, 1, 171, 102, 28, 24, 18, 107, 205, 231, 227, 98, 220,
+            105, 211, 29, 181, 30, 53, 7, 200, 154, 134, 246, 38, 139,
+        ]);
+        let ct_110 = CombinatorialToken([
+            101, 210, 61, 196, 5, 247, 150, 41, 186, 49, 11, 63, 139, 53, 25, 65, 161, 83, 24, 142,
+            225, 102, 57, 241, 199, 18, 226, 137, 68, 3, 219, 131,
+        ]);
 
         assert_ok!(CombinatorialTokens::split_position(
             alice.signed(),
@@ -17,6 +27,9 @@ fn split_followed_by_merge_vertical_no_parent() {
             amount,
         ));
         assert_eq!(alice.free_balance(Asset::Ztg), _99);
+        assert_eq!(alice.free_balance(ct_001), _1);
+        assert_eq!(alice.free_balance(ct_110), _1);
+        assert_eq!(pallet.free_balance(Asset::Ztg), _1);
 
         assert_ok!(CombinatorialTokens::merge_position(
             alice.signed(),
@@ -26,35 +39,95 @@ fn split_followed_by_merge_vertical_no_parent() {
             amount,
         ));
         assert_eq!(alice.free_balance(Asset::Ztg), _100);
+        assert_eq!(alice.free_balance(ct_001), 0);
+        assert_eq!(alice.free_balance(ct_110), 0);
+        assert_eq!(pallet.free_balance(Asset::Ztg), 0);
     });
 }
 
 #[test]
 fn split_followed_by_merge_vertical_with_parent() {
     ExtBuilder::build().execute_with(|| {
-        // let alice = Account::new(0).deposit(Asset::Ztg, _100).unwrap();
+        let alice = Account::new(0).deposit(Asset::Ztg, _100).unwrap();
+        let pallet = Account::new(Pallet::<Runtime>::account_id());
 
-        // let parent_market_id = create_market(Asset::Ztg, MarketType::Categorical(3));
-        // let amount = _1;
-        // let partition = vec![vec![B1, B0, B1], vec![B0, B1, B0]];
+        let ct_001 = CombinatorialToken([
+            207, 168, 160, 93, 238, 221, 197, 1, 171, 102, 28, 24, 18, 107, 205, 231, 227, 98, 220,
+            105, 211, 29, 181, 30, 53, 7, 200, 154, 134, 246, 38, 139,
+        ]);
+        let ct_110 = CombinatorialToken([
+            101, 210, 61, 196, 5, 247, 150, 41, 186, 49, 11, 63, 139, 53, 25, 65, 161, 83, 24, 142,
+            225, 102, 57, 241, 199, 18, 226, 137, 68, 3, 219, 131,
+        ]);
+        let ct_001_0101 = CombinatorialToken([
+            38, 14, 141, 152, 199, 40, 88, 165, 208, 236, 195, 198, 208, 75, 93, 85, 114, 4, 175,
+            225, 211, 72, 142, 210, 98, 202, 168, 193, 245, 217, 239, 28,
+        ]);
+        let ct_001_1010 = CombinatorialToken([
+            107, 142, 3, 38, 49, 137, 237, 239, 1, 131, 197, 221, 236, 46, 246, 93, 185, 197, 228,
+            184, 75, 79, 107, 73, 89, 19, 22, 124, 15, 58, 110, 100,
+        ]);
 
-        // assert_ok!(CombinatorialTokens::split_position(
-        //     alice.signed(),
-        //     None,
-        //     market_id,
-        //     partition.clone(),
-        //     amount,
-        // ));
-        // assert_eq!(alice.free_balance(Asset::Ztg), _99);
+        let parent_market_id = create_market(Asset::Ztg, MarketType::Categorical(3));
+        let parent_amount = _3;
+        let parent_partition = vec![vec![B0, B0, B1], vec![B1, B1, B0]];
+        assert_ok!(CombinatorialTokens::split_position(
+            alice.signed(),
+            None,
+            parent_market_id,
+            parent_partition.clone(),
+            parent_amount,
+        ));
 
-        // assert_ok!(CombinatorialTokens::merge_position(
-        //     alice.signed(),
-        //     None,
-        //     market_id,
-        //     partition,
-        //     amount,
-        // ));
-        // assert_eq!(alice.free_balance(Asset::Ztg), _100);
+        let child_market_id = create_market(Asset::Ztg, MarketType::Categorical(4));
+        let child_amount = _1;
+        // Collection ID of [0, 0, 1].
+        let parent_collection_id = [
+            6, 44, 173, 50, 122, 106, 144, 185, 253, 19, 252, 218, 215, 241, 218, 37, 196, 112, 45,
+            133, 165, 48, 231, 189, 87, 123, 131, 18, 190, 5, 110, 93,
+        ];
+        let child_partition = vec![vec![B0, B1, B0, B1], vec![B1, B0, B1, B0]];
+        assert_ok!(CombinatorialTokens::split_position(
+            alice.signed(),
+            Some(parent_collection_id),
+            child_market_id,
+            child_partition.clone(),
+            child_amount,
+        ));
+        assert_eq!(alice.free_balance(ct_001), parent_amount - child_amount);
+        assert_eq!(alice.free_balance(ct_110), parent_amount);
+        assert_eq!(alice.free_balance(Asset::Ztg), _100 - parent_amount);
+        assert_eq!(alice.free_balance(ct_001_0101), child_amount);
+        assert_eq!(alice.free_balance(ct_001_1010), child_amount);
+        assert_eq!(pallet.free_balance(Asset::Ztg), parent_amount);
+
+        assert_ok!(CombinatorialTokens::merge_position(
+            alice.signed(),
+            Some(parent_collection_id),
+            child_market_id,
+            child_partition,
+            child_amount,
+        ));
+        assert_eq!(alice.free_balance(ct_001), parent_amount);
+        assert_eq!(alice.free_balance(ct_110), parent_amount);
+        assert_eq!(alice.free_balance(Asset::Ztg), _100 - parent_amount);
+        assert_eq!(alice.free_balance(ct_001_0101), 0);
+        assert_eq!(alice.free_balance(ct_001_1010), 0);
+        assert_eq!(pallet.free_balance(Asset::Ztg), parent_amount);
+
+        assert_ok!(CombinatorialTokens::merge_position(
+            alice.signed(),
+            None,
+            parent_market_id,
+            parent_partition,
+            parent_amount,
+        ));
+        assert_eq!(alice.free_balance(ct_001), 0);
+        assert_eq!(alice.free_balance(ct_110), 0);
+        assert_eq!(alice.free_balance(Asset::Ztg), _100);
+        assert_eq!(alice.free_balance(ct_001_0101), 0);
+        assert_eq!(alice.free_balance(ct_001_1010), 0);
+        assert_eq!(pallet.free_balance(Asset::Ztg), 0);
     });
 }
 
