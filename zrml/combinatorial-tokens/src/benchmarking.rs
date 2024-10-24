fe0bf241ffe56a5ab7bbb5500fb8193c0cfe8b6f
Malte Kliemann <mail@maltekliemann.com>
2024-10-24 15:03:25 +0200

    Implement more benchmarks

--- zrml/combinatorial-tokens/src/benchmarking.rs
+++ zrml/combinatorial-tokens/src/benchmarking.rs
@@ -18,7 +18,7 @@
 #![cfg(feature = "runtime-benchmarks")]
 
 use crate::{BalanceOf, Call, Config, Event, MarketIdOf, Pallet};
-use alloc::vec;
+use alloc::{vec, vec::Vec};
 use frame_benchmarking::v2::*;
 use frame_support::{
     dispatch::RawOrigin,
@@ -30,11 +30,9 @@ use sp_runtime::{traits::Zero, Perbill, SaturatedConversion};
 use zeitgeist_primitives::{
     constants::base_multiples::*,
     math::fixed::{BaseProvider, ZeitgeistBase},
-    traits::CombinatorialTokensBenchmarkHelper,
+    traits::{CombinatorialTokensBenchmarkHelper, MarketCommonsPalletApi},
     types::{Asset, Market, MarketCreation, MarketPeriod, MarketStatus, MarketType, ScoringRule},
 };
-use alloc::vec::Vec;
-use zeitgeist_primitives::traits::MarketCommonsPalletApi;
 
 fn create_market<T: Config>(caller: T::AccountId, asset_count: u16) -> MarketIdOf<T> {
     let market = Market {
@@ -117,6 +115,63 @@ mod benchmarks {
             asset_in: position,
             amount_in: amount,
             asset_out: Asset::Ztg,
+            amount_out: amount,
+        });
+        System::<T>::assert_last_event(expected_event.into());
+    }
+
+    #[benchmark]
+    fn redeem_position_with_parent(n: Linear<2, 128>) {
+        let alice: T::AccountId = whitelisted_caller();
+
+        let n_u16: u16 = n.try_into().unwrap();
+        let asset_count = n_u16 + 1;
+
+        // `index_set` has `n` entries that are `true`, which results in `n` iterations in the `for`
+        // loop in `redeem_position`.
+        let mut index_set = vec![true; asset_count as usize];
+        *index_set.last_mut().unwrap() = false;
+
+        let parent_market_id = create_market::<T>(alice.clone(), 2);
+        let cid_01 = Pallet::<T>::collection_id_from_parent_collection(
+            None,
+            parent_market_id,
+            vec![false, true],
+            false,
+        ).unwrap();
+        let pos_01 = Pallet::<T>::position_from_collection_id(parent_market_id, cid_01).unwrap();
+
+        let child_market_id = create_market::<T>(alice.clone(), asset_count);
+        let pos_01_10 = Pallet::<T>::position_from_parent_collection(
+            Some(cid_01),
+            child_market_id,
+            index_set.clone(),
+            false,
+        )
+        .unwrap();
+        let amount = ZeitgeistBase::get().unwrap();
+        T::MultiCurrency::deposit(pos_01_10, &alice, amount).unwrap();
+
+        let payout_vector = create_payout_vector::<T>(asset_count);
+        T::BenchmarkHelper::setup_payout_vector(parent_market_id, Some(payout_vector)).unwrap();
+
+        #[extrinsic_call]
+        redeem_position(
+            RawOrigin::Signed(alice.clone()),
+            Some(cid_01),
+            child_market_id,
+            index_set.clone(),
+            true,
+        );
+
+        let expected_event = <T as Config>::RuntimeEvent::from(Event::<T>::TokenRedeemed {
+            who: alice,
+            parent_collection_id: Some(cid_01),
+            market_id: child_market_id,
+            index_set,
+            asset_in: pos_01_10,
+            amount_in: amount,
+            asset_out: pos_01,
             amount_out: amount,
         });
         System::<T>::assert_last_event(expected_event.into());
