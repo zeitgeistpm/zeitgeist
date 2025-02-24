929df7d20724790d40c0d4a098ae37444f217d37
Chralt98 <chralt98@gmail.com>
2025-02-24 12:19:08 +0100

    fix copyrights

--- zrml/futarchy/src/traits/proposal_storage.rs
+++ zrml/futarchy/src/traits/proposal_storage.rs
@@ -1,4 +1,21 @@
-// Copyright 2025 Forecasting Technologies LTD.
+// Copyright 2024-2025 Forecasting Technologies LTD.
+//
+// This file is part of Zeitgeist.
+//
+// Zeitgeist is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at
+// your option) any later version.
+//
+// Zeitgeist is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Zeitgeist. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::{types::Proposal, Config, ProposalsOf};
 use alloc::{collections::BTreeMap, vec::Vec};
 use frame_system::pallet_prelude::BlockNumberFor;
 use sp_runtime::DispatchError;
