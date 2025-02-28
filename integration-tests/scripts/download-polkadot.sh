b60abc5ee61e6cf1050a44288f05a4c8d039bb2d
Chralt98 <chralt98@gmail.com>
2025-02-28 13:22:08 +0100

    use polkadot 1.1.0 for zombienet tests and update endpoint

--- integration-tests/scripts/download-polkadot.sh
+++ integration-tests/scripts/download-polkadot.sh
@@ -27,10 +27,12 @@ if [[ $(uname) == "Darwin" ]]; then
     exit 1
 fi
 
-# TODO: Use following line instead of the one above once Zeitgeist uses >=polkadot-v1.1.0
+# TODO: Use following line instead of the one above once Zeitgeist uses >=polkadot-v1.1.0, and also switch to the moonwall downloader then again
 # Note: Version 1.4.0 of relaychain didn't allow the parachain to produce blocks
 # branch=$(egrep -o '/polkadot.*#([^\"]*)' $(dirname $0)/../../Cargo.lock | head -1 | sed 's/.*release-//#')
 # polkadot_release=$(echo $branch | sed 's/#.*//' | sed 's/\/polkadot-sdk?branch=polkadot-v//' | sed 's/-.*//')
+# moonwall doesn't offer release 1.1.0 anymore (look at `pnpm moonwall` and navigate to artifact downloader)
+# however polkadot release 1.7.1, which is the oldest release available, doesn't allow the parachain to produce blocks
 polkadot_release="1.1.0"
 
 # Always run the commands from the "integration-tests" dir
@@ -44,24 +46,30 @@ if [[ -f tmp/polkadot ]]; then
 	else
 		echo "Updating polkadot binary..."
 
-		pnpm moonwall download polkadot $polkadot_release tmp
+		# pnpm moonwall download polkadot $polkadot_release tmp
+		curl -L -o $(dirname $0)/../tmp/polkadot https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.1.0/polkadot
 		chmod +x tmp/polkadot
 
-		pnpm moonwall download polkadot-execute-worker $polkadot_release tmp
+		# pnpm moonwall download polkadot-execute-worker $polkadot_release tmp
+		curl -L -o $(dirname $0)/../tmp/polkadot-execute-worker https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.1.0/polkadot-execute-worker
 		chmod +x tmp/polkadot-execute-worker
 
-		pnpm moonwall download polkadot-prepare-worker $polkadot_release tmp
+		# pnpm moonwall download polkadot-prepare-worker $polkadot_release tmp
+		curl -L -o $(dirname $0)/../tmp/polkadot-prepare-worker https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.1.0/polkadot-prepare-worker
 		chmod +x tmp/polkadot-prepare-worker
 
 	fi
 else
 	echo "Polkadot binary not found, downloading..."
-	pnpm moonwall download polkadot $polkadot_release tmp
+	# pnpm moonwall download polkadot $polkadot_release tmp
+	curl -L -o $(dirname $0)/../tmp/polkadot https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.1.0/polkadot
 	chmod +x tmp/polkadot
 
-	pnpm moonwall download polkadot-execute-worker $polkadot_release tmp
+	# pnpm moonwall download polkadot-execute-worker $polkadot_release tmp
+	curl -L -o $(dirname $0)/../tmp/polkadot-execute-worker https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.1.0/polkadot-execute-worker
 	chmod +x tmp/polkadot-execute-worker
 
-	pnpm moonwall download polkadot-prepare-worker $polkadot_release tmp
+	# pnpm moonwall download polkadot-prepare-worker $polkadot_release tmp
+	curl -L -o $(dirname $0)/../tmp/polkadot-prepare-worker https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v1.1.0/polkadot-prepare-worker
 	chmod +x tmp/polkadot-prepare-worker
 fi
