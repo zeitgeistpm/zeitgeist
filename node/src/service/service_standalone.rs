9271312a5b801172f378637ee373a6faf5d76144
Chralt98 <chralt98@gmail.com>
2025-05-08 11:42:36 +0200

    remove additional new_full_parts from storage weight reclaim

--- node/src/service/service_standalone.rs
+++ node/src/service/service_standalone.rs
@@ -342,11 +342,10 @@ where
 
     let executor = sc_service::new_wasm_executor::<sp_io::SubstrateHostFunctions>(&config.executor);
     let (client, backend, keystore_container, task_manager) =
-        sc_service::new_full_parts_record_import::<Block, RuntimeApi, _>(
+        sc_service::new_full_parts::<Block, RuntimeApi, _>(
             &config,
             telemetry.as_ref().map(|(_, telemetry)| telemetry.handle()),
             executor,
-            true,
         )?;
     let client = Arc::new(client);
 
