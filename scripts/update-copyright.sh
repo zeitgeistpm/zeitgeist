89f5522014f932fb9b008b7efe5447dc0ad7a4e0
Chralt98 <chralt98@gmail.com>
2025-11-25 14:08:37 +0100

    Enhance compliance checks by adding Node.js setup and pnpm support; update license-checker and audit commands

--- scripts/update-copyright.sh
+++ scripts/update-copyright.sh
@@ -1,3 +1,5 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
+#!/usr/bin/env bash
+
 RUST_FILES_CHANGED_NOT_DELETED=$(git diff --diff-filter=d --name-only main | grep -E '.*\.rs$')
 check-license -w ${RUST_FILES_CHANGED_NOT_DELETED}
