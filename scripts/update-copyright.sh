14590d7c06bd5d8bdca1e336ca07ea8197c93b19
Chralt98 <chralt98@gmail.com>
2025-11-26 11:28:21 +0100

    Enhance gitleaks installation in compliance workflow with dynamic version retrieval and architecture support

--- scripts/update-copyright.sh
+++ scripts/update-copyright.sh
@@ -1,5 +1,5 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
 #!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 RUST_FILES_CHANGED_NOT_DELETED=$(git diff --diff-filter=d --name-only main | grep -E '.*\.rs$')
 check-license -w ${RUST_FILES_CHANGED_NOT_DELETED}
